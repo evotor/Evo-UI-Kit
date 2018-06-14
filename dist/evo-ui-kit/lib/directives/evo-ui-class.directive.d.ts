5a380e93eaafe18df344074fd36a4cd8e3496ee1
Nikita Kozlov <kozlovzxc@gmail.com>
2018-06-14 11:03:07 +0300

    remove dist

--- dist/evo-ui-kit/lib/directives/evo-ui-class.directive.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef, IterableDiffers, KeyValueDiffers, Renderer2 } from '@angular/core';
-import { NgClass } from '@angular/common';
-export declare class EvoUiClassDirective extends NgClass {
-    setterOfClass: any;
-    ngClass: any;
-    private prefix;
-    constructor(_iterableDiffers: IterableDiffers, _keyValueDiffers: KeyValueDiffers, _ngEl: ElementRef, _renderer: Renderer2);
-}
--- dist/evo-ui-kit/lib/evo-ui-kit.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export { EvoSidebarService } from './components/evo-sidebar/evo-sidebar.service';
-export declare class EvoUiKitModule {
-    static forRoot(): ModuleWithProviders;
-}
