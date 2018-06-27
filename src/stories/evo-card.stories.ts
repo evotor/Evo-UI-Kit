63346160d3c9737480bcd016709d831195d699f2
Nikita Kozlov <kozlovzxc@gmail.com>
2018-06-27 19:40:01 +0300

    add flex grow to card content, fix sidebar storie

--- src/stories/evo-card.stories.ts
+++ src/stories/evo-card.stories.ts
@@ -1,7 +1,14 @@
-import { storiesOf } from '@storybook/angular';
+import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { EvoUiKitModule } from 'evo-ui-kit';
 import '!style-loader!css-loader!sass-loader!evo-ui-kit/styles/main.scss';
+import '!style-loader!css-loader!sass-loader!./evo-card.stories.scss';
 
 storiesOf('Typography/EvoCard', module)
+  .addDecorator(
+    moduleMetadata({
+      imports: [EvoUiKitModule],
+    }),
+  )
   .add('default', () => ({
     template: `
     <div class="evo-cards">
@@ -17,7 +24,31 @@ storiesOf('Typography/EvoCard', module)
     template: `
     <div class="evo-cards">
       <div class="evo-card">
-        <div class="evo-card__title"><img src="/assets/sberbank.png"/></div>
+        <div class="evo-card__title"><img src="/sberbank.png"/></div>
+        <div class="evo-card__content">Содержание</div>
+        <div class="evo-card__footer"><evo-button size="full-width">Действие!</evo-button></div>
+      </div>
+    </div>
+    `,
+  }))
+  .add('multiple cards', () => ({
+    template: `
+    <div class="multiple-cards">
+      <div class="evo-card">
+        <div class="evo-card__title">Заголовок</div>
+        <div class="evo-card__content">
+          <ul>
+            <li>Элемент списка</li>
+            <li>Элемент списка</li>
+            <li>Элемент списка</li>
+            <li>Элемент списка</li>
+            <li>Элемент списка</li>
+          </ul>
+        </div>
+        <div class="evo-card__footer"><evo-button size="full-width">Действие!</evo-button></div>
+      </div>
+      <div class="evo-card">
+        <div class="evo-card__title">Заголовок</div>
         <div class="evo-card__content">Содержание</div>
         <div class="evo-card__footer"><evo-button size="full-width">Действие!</evo-button></div>
       </div>
