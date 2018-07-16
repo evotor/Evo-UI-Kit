621ab99af3cedb0a6f76d9ade73640c1fcc8419d
Trebushnoi <treb-slava@list.ru>
2018-07-16 13:02:11 +0300

    ~ EVUIK-34. Revomed dist

--- dist/evo-ui-kit/lib/evo-ui-kit.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { EvoSidebarService } from './components/evo-sidebar/evo-sidebar.service';
-export { EvoSidebarService };
-export declare class EvoUiKitModule {
-    static forRoot(): ModuleWithProviders;
-}
