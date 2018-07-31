6bc18157b2af07a223b22c246f381250a5a4a274
Nikita Kozlov <kozlovzxc@gmail.com>
2018-07-31 13:14:41 +0300

    add style=inline to evo-select

--- src/stories/evo-select.stories.ts
+++ src/stories/evo-select.stories.ts
@@ -1,6 +1,12 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { withKnobs, text } from '@storybook/addon-knobs/angular';
+import { withKnobs, text, select } from '@storybook/addon-knobs/angular';
 import { EvoUiKitModule } from 'evo-ui-kit';
+
+const options = [
+    { label: 'Все сотрудники', value: 'all' },
+    { label: 'Илья Лыткин', value: 'i.lytkin' },
+    { label: 'Кристина Михайлов', value: 'k.mykhaylova' },
+];
 
 storiesOf('Components/EvoSelect', module)
   .addDecorator(
@@ -16,27 +22,35 @@ storiesOf('Components/EvoSelect', module)
     </evo-select>
     `,
     props: {
-        options: [
-            { label: 'Все сотрудники', value: 'all' },
-            { label: 'Илья Лыткин', value: 'i.lytkin' },
-            { label: 'Кристина Михайлов', value: 'k.mykhaylova' },
-        ],
+        options,
     },
   }))
   .add('with label', () => ({
     template: `
+    <style>
+        .content {
+            display: block;
+        }
+    </style>
     <evo-control-label [label]="label">
-        <evo-select>
+        <evo-select class="content">
             <option *ngFor="let option of options" [value]="option.value">{{option.label}}</option>
         </evo-select>
     </evo-control-label>
     `,
     props: {
-        options: [
-            { label: 'Все сотрудники', value: 'all' },
-            { label: 'Илья Лыткин', value: 'i.lytkin' },
-            { label: 'Кристина Михайлов', value: 'k.mykhaylova' },
-        ],
+        options,
         label: text('label', 'Сортировка'),
+    },
+  }))
+  .add('with style', () => ({
+    template: `
+    <evo-select [style]="style">
+        <option *ngFor="let option of options" [value]="option.value">{{option.label}}</option>
+    </evo-select>
+    `,
+    props: {
+        options,
+        style: select('style', [ 'inline', 'input' ], 'input'),
     },
   }));
