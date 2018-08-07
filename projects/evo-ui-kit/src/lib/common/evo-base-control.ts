ac9c19d9455ea9a4fc55eed57c8590942baf0e4e
Kozlov Nikita <kozlovzxc@users.noreply.github.com>
2018-08-07 16:42:50 +0300

    Revert "show error only on template driven forms"

--- projects/evo-ui-kit/src/lib/common/evo-base-control.ts
+++ projects/evo-ui-kit/src/lib/common/evo-base-control.ts
@@ -6,52 +6,34 @@ import { EvoControlStates } from './evo-control-state-manager/evo-control-states
 
 export class EvoBaseControl implements AfterContentInit {
 
-    @ContentChild(FormControlName) formControlName: FormControlName;
-    @ContentChild(FormControlDirective) formControlDirective: FormControlDirective;
+  @ContentChild(FormControlName) formControlName: FormControlName;
+  @ContentChild(FormControlDirective) formControlDirective: FormControlDirective;
 
-    @Input() errorsMessages: IEvoControlError;
-    @Input() state: IEvoControlState;
+  @Input() errorsMessages: IEvoControlError;
+  @Input() state: IEvoControlState;
 
-    control: AbstractControl;
-    __proto__: any;
-    private haveValue = false;
+  control: AbstractControl;
 
-    constructor() {
-        const origWriteValue = this.__proto__.writeValue;
-        this.__proto__.writeValue = function() {
-            this.haveValue = true;
-            origWriteValue.apply(this, arguments);
-        };
-
-        const origNgAfterContentInit = this.__proto__.ngAfterContentInit;
-        this.__proto__.ngAfterContentInit = function() {
-            if (this.formControlName && this.formControlName.control) {
-                this.control = this.formControlName.control;
-            } else if (this.formControlDirective && this.formControlDirective.control) {
-                this.control = this.formControlDirective.control;
-            }
-            if (!this.control && this.haveValue) {
-                throw new Error('No template driven forms allowed with EvoUi kit');
-            }
-            origNgAfterContentInit.apply(this, arguments);
-        };
+  ngAfterContentInit() {
+    if (this.formControlName && this.formControlName.control) {
+      this.control = this.formControlName.control;
+    } else if (this.formControlDirective && this.formControlDirective.control) {
+      this.control = this.formControlDirective.control;
     }
+  }
 
-    ngAfterContentInit() {
-    }
+  get currentState(): IEvoControlState {
+    return Object.assign(
+        {
+            valid: this.control ? this.control.dirty && this.control.touched && this.control.valid : undefined,
+            invalid: this.control ? this.control.dirty && this.control.touched && this.control.invalid : undefined,
+        },
+        this.state,
+    );
+  }
 
-    get currentState(): IEvoControlState {
-        return Object.assign(
-            {
-                valid: this.control ? this.control.dirty && this.control.touched && this.control.valid : undefined,
-                invalid: this.control ? this.control.dirty && this.control.touched && this.control.invalid : undefined,
-            },
-            this.state,
-        );
-    }
-
-    get showErrors(): boolean {
-        return this.currentState[EvoControlStates.invalid];
-    }
+  get showErrors(): boolean {
+    return this.currentState[EvoControlStates.invalid];
+  }
 
 }
