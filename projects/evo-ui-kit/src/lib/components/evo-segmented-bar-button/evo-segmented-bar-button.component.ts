059c64e0270c3468a5a05875524d4dce7e481ab8
Andrey Matveev <andrey.g.matveev@gmail.com>
2018-09-04 13:21:15 +0700

    * extend public api + evo-segmented-bar, evo-segmented-bar-button components + evo-segmented-bar storybook

---
+++ projects/evo-ui-kit/src/lib/components/evo-segmented-bar-button/evo-segmented-bar-button.component.ts
@@ -0,0 +1,56 @@
+import { Component, ContentChild, ElementRef, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+    selector: 'evo-segmented-bar-button',
+    templateUrl: './evo-segmented-bar-button.component.html',
+    styleUrls: [ './evo-segmented-bar-button.component.scss' ],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => EvoSegmentedBarButtonComponent),
+            multi: true,
+        },
+    ],
+})
+export class EvoSegmentedBarButtonComponent implements ControlValueAccessor, OnInit {
+    @Input() name: string;
+    @Input() value: string;
+
+    @ContentChild('counter') counter: ElementRef;
+
+    private _selectedValue: any;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        if (this.counter) {
+            console.log(this.counter);
+        }
+    }
+
+    onChange = (_) => {};
+    onTouched = () => {};
+
+    get selectedValue() {
+        return this._selectedValue;
+    }
+
+    writeValue(value) {
+        this._selectedValue = value;
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
+    }
+
+    onInputChange(value) {
+        this._selectedValue = value;
+        this.onChange(value);
+    }
+}
