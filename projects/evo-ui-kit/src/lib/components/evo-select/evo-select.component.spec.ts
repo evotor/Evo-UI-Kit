a94e4600fe05aee7c862ecea0ceffede5f793afa
Vadim Petrov <pv@petrovvadim.com>
2018-12-05 18:08:33 +0300

    test(popover.component): few tests added

--- projects/evo-ui-kit/src/lib/components/evo-select/evo-select.component.spec.ts
+++ projects/evo-ui-kit/src/lib/components/evo-select/evo-select.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EvoSelectComponent } from './evo-select.component';
+import { EvoUiClassDirective } from '../../evo-ui-kit.module';
+import { FormsModule } from '@angular/forms';
+import { ViewChild, Component } from '@angular/core';
+
+@Component({
+  selector: 'evo-host-component',
+  template: `
+  <evo-select [style]="'inline'">
+    <option *ngFor="let option of options" [value]="option.value">{{ option.label }}</option>
+  </evo-select>`,
+})
+class TestHostComponent {
+  options = [
+    { label: 'Все сотрудники', value: 'all' },
+    { label: 'Илья Лыткин', value: 'i.lytkin' },
+    { label: 'Кристина Михайлова', value: 'k.mykhaylova' },
+    { label: 'Аааааааааааааааааааааааа', value: 'panic' },
+  ];
+  @ViewChild(EvoSelectComponent)
+  public selectComponent: EvoSelectComponent;
+}
 
 describe('EvoSelectComponent', () => {
-  let component: EvoSelectComponent;
-  let fixture: ComponentFixture<EvoSelectComponent>;
+  let testHostComponent: EvoSelectComponent;
+  let testHostFixture: ComponentFixture<EvoSelectComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EvoSelectComponent ],
-    })
-    .compileComponents();
+      imports: [ FormsModule ],
+      declarations: [ EvoSelectComponent, EvoUiClassDirective ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EvoSelectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHostFixture = TestBed.createComponent(EvoSelectComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    testHostFixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(testHostComponent).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  title = 'app';
-
-  constructor() {
-  }
-}
+export class AppComponent {}
