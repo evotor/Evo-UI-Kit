9efaba367348cdb1ab09df1b30b885fdc080d955
Vadim Petrov <pv@petrovvadim.com>
2019-01-17 17:59:16 +0300

    test(button): reworked

--- projects/evo-ui-kit/src/lib/components/evo-button/evo-button.component.spec.ts
+++ projects/evo-ui-kit/src/lib/components/evo-button/evo-button.component.spec.ts
@@ -1,71 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EvoUiClassDirective, EvoButtonComponent } from '../../evo-ui-kit.module';
-import { Component, ViewChild } from '@angular/core';
-
-@Component({
-    selector: 'evo-host-component',
-    template: `
-    <button evo-button
-        [size]="size"
-        [color]="color"
-        [disabled]="disabled"
-        [loading]="loading"
-        >Some button</button>`,
-})
-class TestHostComponent {
-    disabled: boolean;
-    loading: boolean;
-    color: string;
-    size: string;
-
-    @ViewChild(EvoButtonComponent)
-    public tabsComponent: EvoButtonComponent;
-}
+import { EvoButtonSizes, EvoButtonStyles } from './evo-button.component';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('EvoButtonComponent', () => {
-    let testHostComponent: TestHostComponent;
-    let testHostFixture: ComponentFixture<TestHostComponent>;
+    let component: EvoButtonComponent;
+    let fixture: ComponentFixture<EvoButtonComponent>;
+    let buttonEl: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                TestHostComponent,
-                EvoUiClassDirective,
                 EvoButtonComponent,
+                EvoUiClassDirective,
             ],
+        }).overrideComponent(EvoButtonComponent, {
+            set: { changeDetection: ChangeDetectionStrategy.Default },
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        testHostFixture = TestBed.createComponent(TestHostComponent);
-        testHostComponent = testHostFixture.componentInstance;
-        testHostFixture.detectChanges();
+        fixture = TestBed.createComponent(EvoButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        buttonEl = fixture.nativeElement.querySelector('.evo-button');
     });
 
-    it(`should be small if input size = 'small'`, () => {
-        testHostComponent.size = 'small';
-        testHostFixture.detectChanges();
-        expect(testHostFixture.nativeElement
-            .querySelector('.evo-button').classList.contains('evo-button_small')).toBeTruthy();
+    it(`should be small if input size = ${EvoButtonSizes.small}`, () => {
+        component.size = EvoButtonSizes.small;
+        fixture.detectChanges();
+        expect(buttonEl.classList.contains('evo-button_small')).toBeTruthy();
     });
 
-    it(`should be darkblue if input color = 'darkblue'`, () => {
-        testHostComponent.color = 'darkblue';
-        testHostFixture.detectChanges();
-        expect(testHostFixture.nativeElement
-            .querySelector('.evo-button').classList.contains('evo-button_darkblue')).toBeTruthy();
+    it(`should be darkblue if input color = ${EvoButtonStyles.darkblue}`, () => {
+        component.color = EvoButtonStyles.darkblue;
+        fixture.detectChanges();
+        expect(buttonEl.classList.contains('evo-button_darkblue')).toBeTruthy();
     });
 
     it(`should be disabled if input disabled = true`, () => {
-        testHostComponent.disabled = true;
-        testHostFixture.detectChanges();
-        expect(testHostFixture.nativeElement.querySelector('button').disabled).toBeTruthy();
+        component.disabled = true;
+        fixture.detectChanges();
+        expect(buttonEl.classList.contains('evo-button_disabled')).toBeTruthy();
     });
 
     it(`should contain dotts(spinner) if input loading = true`, () => {
-        testHostComponent.loading = true;
-        testHostFixture.detectChanges();
-        expect(testHostFixture.nativeElement.querySelector('.evo-button__dots') != null).toBeTruthy();
+        component.loading = true;
+        fixture.detectChanges();
+        expect(buttonEl.querySelector('.evo-button__dots') != null).toBeTruthy();
     });
 
 });
