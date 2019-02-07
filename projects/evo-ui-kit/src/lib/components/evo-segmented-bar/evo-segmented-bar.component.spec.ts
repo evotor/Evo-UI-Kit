4fa5345c97ac443004d6b9f06bd4dbd3f678d08c
Vadim Petrov <pv@petrovvadim.com>
2019-02-07 13:52:08 +0300

    test(segmented-bar): +few tests

--- projects/evo-ui-kit/src/lib/components/evo-segmented-bar/evo-segmented-bar.component.spec.ts
+++ projects/evo-ui-kit/src/lib/components/evo-segmented-bar/evo-segmented-bar.component.spec.ts
@@ -1,5 +1,5 @@
 import { createHostComponentFactory, SpectatorWithHost } from '@netbasal/spectator';
-import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { EvoSegmentedBarComponent, EvoSegmentedBarButtonComponent, EvoUiClassDirective } from '../../evo-ui-kit.module';
 
@@ -7,23 +7,19 @@ const optionsList = [ {
     label: 'Все',
     value: 'all',
     counter: 14,
-},
-{
+}, {
     label: 'Поддержка',
     value: 'support',
     counter: 12,
-},
-{
+}, {
     label: 'Оплата',
     value: 'payment',
     counter: 2,
-},
-{
+}, {
     label: 'Зарплата',
     value: 'cash',
     counter: 5,
-},
-{
+}, {
     label: 'Штрафы',
     value: 'fines',
     counter: 2,
@@ -33,10 +29,13 @@ const optionsList = [ {
 class TestHostComponent {
     optionsList = optionsList;
     selectedValue = 'all';
+    label = 'Label';
+    labelShort = 'Label short';
 }
 
 describe('EvoSegmentedBarComponent', () => {
     let host: SpectatorWithHost<EvoSegmentedBarComponent, TestHostComponent>;
+    let hostComponent: TestHostComponent;
     const createHost = createHostComponentFactory({
         component: EvoSegmentedBarComponent,
         declarations: [ EvoSegmentedBarButtonComponent, EvoUiClassDirective ],
@@ -47,15 +46,17 @@ describe('EvoSegmentedBarComponent', () => {
 
     beforeEach(() => {
         host = createHost(`
-            <evo-segmented-bar label="Options label:">
+            <evo-segmented-bar [label]="label" [labelShort]="labelShort">
                 <evo-segmented-bar-button
                     *ngFor="let option of optionsList"
                     name="filterList"
                     [value]="option.value"
                     [(ngModel)]="selectedValue">
-                    {{ option.label }}
+                    <span>{{ option.label }}</span>
+                    <span data-type="counter" *ngIf="option.counter">{{ option.counter }}</span>
                 </evo-segmented-bar-button>
             </evo-segmented-bar>`);
+        hostComponent = host.hostComponent;
         inputEls = host.queryAll('.segmented-button__input');
     });
 
@@ -63,8 +64,20 @@ describe('EvoSegmentedBarComponent', () => {
         expect(inputEls.length).toEqual(optionsList.length);
     });
 
+    it(`should have specified labels`, () => {
+        const labelEl = host.query('.bar__label_normal');
+        const labelShortEl = host.query('.bar__label_short');
+        expect(labelEl).toHaveText(hostComponent.label);
+        expect(labelShortEl).toHaveText(hostComponent.labelShort);
+    });
+
     it(`should have selected segment with label = ${optionsList[2].label} after click`, () => {
         host.click(inputEls[2]);
         expect(inputEls[2]).toBeChecked();
+    });
+
+    it(`should have specified counter`, () => {
+        const counterEl = host.query('.segmented-button__view-text span[data-type="counter"]');
+        expect(counterEl).toHaveText(hostComponent.optionsList[0].counter.toString());
     });
 });
