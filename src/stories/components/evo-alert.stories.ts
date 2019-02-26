c2f1046ef8a224b4a001fa37323f72526c70cde9
Trebushnoi <treb-slava@list.ru>
2019-02-26 14:41:23 +0300

    feat(evo-alert): removed legacy design

--- src/stories/components/evo-alert.stories.ts
+++ src/stories/components/evo-alert.stories.ts
@@ -12,25 +12,22 @@ storiesOf('Components/Alert', module)
   )
   .add('default', () => ({
     template: `
-        <h3 style="margin: 20px;"> Мы находимся в процессе мягкого перехода на новый дизайн алертов.
-            Чтобы не сломать старый вид алертов, в новых нужно добавлять параметр <b>version="current"</b>
-        </h3>
-        <evo-alert version="current">Содержимое уведомления типа <strong>Успех (success, по умолчанию)</strong></evo-alert>
-        <evo-alert version="current" type="warning">Содержимое уведомления типа <strong>Предупреждение (warning)</strong></evo-alert>
-        <evo-alert version="current" type="danger">Содержимое уведолмения типа <strong>Ошибка (danger)</strong></evo-alert>
+        <evo-alert>Содержимое уведомления типа <strong>Успех (success, по умолчанию)</strong></evo-alert>
+        <evo-alert type="warning">Содержимое уведомления типа <strong>Предупреждение (warning)</strong></evo-alert>
+        <evo-alert type="danger">Содержимое уведолмения типа <strong>Ошибка (danger)</strong></evo-alert>
         `,
   }))
   .add('with default icon and size', () => ({
     template: `
-        <evo-alert version="current" icon="success">Успех</evo-alert> <br/>
-        <evo-alert version="current" icon="exclamation" type="danger">Ошибка</evo-alert> <br/>
-        <evo-alert version="current" icon="success" size="large">Успех</evo-alert> <br/>
-        <evo-alert version="current" icon="exclamation" size="large" type="danger">Ошибка</evo-alert> <br/>
+        <evo-alert icon="success">Успех</evo-alert> <br/>
+        <evo-alert icon="exclamation" type="danger">Ошибка</evo-alert> <br/>
+        <evo-alert icon="success" size="large">Успех</evo-alert> <br/>
+        <evo-alert icon="exclamation" size="large" type="danger">Ошибка</evo-alert> <br/>
         `,
   }))
   .add('with custom icon URL', () => ({
     template: `
-          <evo-alert version="current" type="success" iconSrc="https://evotor.ru/app/themes/evotor-main/dist/img/57.svg">
+          <evo-alert type="success" iconSrc="https://evotor.ru/app/themes/evotor-main/dist/img/57.svg">
             Использовать <code>iconSrc</code> вместо дефолтного типа иконки <code>icon</code>
           </evo-alert>
           `,
@@ -38,7 +35,6 @@ storiesOf('Components/Alert', module)
   .add('with close icon', () => ({
     template: `
             <evo-alert
-                version="current"
                 icon="success"
                 [closable]="true"
                 (close)="alertCloseAction()"
@@ -48,12 +44,4 @@ storiesOf('Components/Alert', module)
     props: {
       alertCloseAction: action('evo-alert close action'),
     },
-  }))
-  .add('legacy', () => ({
-    template: `
-        <evo-alert icon="success">Успех</evo-alert> <br/>
-        <evo-alert icon="exclamation" type="danger">Ошибка</evo-alert> <br/>
-        <evo-alert icon="success" size="large">Успех</evo-alert> <br/>
-        <evo-alert icon="exclamation" size="large" type="danger">Ошибка</evo-alert> <br/>
-          `,
   }));
