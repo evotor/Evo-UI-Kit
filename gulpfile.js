95738559920bb094336addf0d6dcfacfe0c09877
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-02-27 23:49:57 +0700

    chore(evo-ui-kit): release fixes, version up

--- gulpfile.js
+++ gulpfile.js
@@ -13,51 +13,57 @@ const storybookDistPath = path.join(__dirname, 'dist', 'storybook');
 const storybookSrcPath = path.join(__dirname, 'src')
 
 const postcssOptions = {
-  parser: parser
+    parser: parser
 };
 
 const inlineURL = () => gulp.src(path.join(srcPath, 'lib/styles/**/*.scss'))
-  .pipe(postcss([
-      url({
-        url: 'inline',
-        encodeType: 'base64',
-      }),
-    ],
-    postcssOptions
-  ))
-  .pipe(gulp.dest(path.join(distPath, 'styles')));
+    .pipe(postcss([
+            url({
+                url: 'inline',
+                encodeType: 'base64',
+            }),
+        ],
+        postcssOptions
+    ))
+    .pipe(gulp.dest(path.join(distPath, 'styles')));
 
 const createSymlink = () => !fs.existsSync(nodeModulesPath) ? fs.symlinkSync(
     distPath,
     nodeModulesPath,
     'dir'
-  ) : undefined;
+) : undefined;
 
 const buildUIKit = () => childProcess.execSync('ng build evo-ui-kit --prod', {stdio: 'inherit'});
 
 const copyReadme = () => fs.copyFileSync(
-  path.join(__dirname, 'README.md'),
-  path.join(distPath, 'README.md')
+    path.join(__dirname, 'README.md'),
+    path.join(distPath, 'README.md')
+);
+
+const copyReleaserc = () => fs.copyFileSync(
+    path.join(srcPath, '..', 'package-releaserc.json'),
+    path.join(distPath, '.releaserc.json')
 );
 
 const buildStorybook = () => childProcess.execSync(`build-storybook -c .storybook -o ${storybookDistPath}`, {stdio: 'inherit'});
 
 const copyStorybookAssets = () => gulp.src(path.join(storybookSrcPath, 'assets/*'))
-  .pipe(gulp.dest(storybookDistPath))
+    .pipe(gulp.dest(storybookDistPath))
 
 gulp.task('link', () => {
-  createSymlink();
+    createSymlink();
 });
 
 gulp.task('storybook', () => {
-  createSymlink();
-  buildStorybook();
-  copyStorybookAssets();
+    createSymlink();
+    buildStorybook();
+    copyStorybookAssets();
 });
 
 gulp.task('default', () => {
-  buildUIKit();
-  inlineURL();
-  createSymlink();
-  copyReadme();
+    buildUIKit();
+    inlineURL();
+    createSymlink();
+    copyReleaserc();
+    copyReadme();
 });
