43d29bf93f9cce0cd4ea002f6d24aea246562b3d
Vadim Petrov <pv@petrovvadim.com>
2019-04-16 12:32:48 +0300

    test(toggle): fix popover story

--- src/stories/components/evo-popover.stories.ts
+++ src/stories/components/evo-popover.stories.ts
@@ -8,11 +8,13 @@ storiesOf('Components/Popover', module)
         }),
     ).add('default', () => ({
         template:
-        `<evo-popover [position]="'right'" [media-tablet-position]="'left'">
-            <p style="max-width: 360px">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-            <p popover-body>Some popover text here...<br>
-                <a href="https://evotor.ru" target="_blank">Some link</a>
-            </p>
-        </evo-popover>`,
+        `<div style="height: 100vh">
+            <evo-popover [position]="'right'" [media-tablet-position]="'left'">
+                <p style="max-width: 360px">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p popover-body>Some popover text here...<br>
+                    <a href="https://evotor.ru" target="_blank">Some link</a>
+                </p>
+            </evo-popover>
+        </div>`,
     }));
