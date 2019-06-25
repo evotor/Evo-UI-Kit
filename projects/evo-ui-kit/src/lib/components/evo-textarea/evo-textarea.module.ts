27c622a9b2bab112fe985f306fbd97a5f22537e3
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-06-25 20:17:34 +0700

    fix(evo-textarea): fix module import

--- projects/evo-ui-kit/src/lib/components/evo-textarea/evo-textarea.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-textarea/evo-textarea.module.ts
@@ -1,8 +1,7 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EvoTextareaComponent } from './evo-textarea.component';
-import { EvoControlErrorModule } from '../evo-control-error';
+import { EvoControlErrorModule } from '../evo-control-error/evo-control-error.module';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
