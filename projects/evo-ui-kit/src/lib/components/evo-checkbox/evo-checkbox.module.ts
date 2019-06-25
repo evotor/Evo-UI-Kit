b79bf5097c77dabf05649aa103cc5b3ffdea4720
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-06-25 21:42:18 +0700

    fix(evo-checkbox): fix module import

--- projects/evo-ui-kit/src/lib/components/evo-checkbox/evo-checkbox.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-checkbox/evo-checkbox.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EvoCheckboxComponent } from './evo-checkbox.component';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
-import { EvoControlErrorModule } from '../evo-control-error';
+import { EvoControlErrorModule } from '../evo-control-error/evo-control-error.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
