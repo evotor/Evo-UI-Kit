ba31c153f612ec9190994212672b2c702c91f5ba
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-08-01 23:18:45 +0700

    fix(evo-select.module): fix import

--- projects/evo-ui-kit/src/lib/components/evo-select/evo-select.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-select/evo-select.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { EvoSelectComponent } from './evo-select.component';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
 import { FormsModule } from '@angular/forms';
-import { EvoControlErrorModule } from '../evo-control-error';
+import { EvoControlErrorModule } from '../evo-control-error/evo-control-error.module';
 
 @NgModule({
     imports: [
