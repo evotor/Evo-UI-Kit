441600c024fc8bf7497b7e2b4949e29a7fc1c964
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-08-01 12:10:00 +0700

    fix(evo-sidebar): fix missing imports

--- projects/evo-ui-kit/src/lib/components/evo-sidebar/evo-sidebar.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-sidebar/evo-sidebar.module.ts
@@ -3,22 +3,24 @@ import { CommonModule } from '@angular/common';
 import { EvoSidebarComponent } from './evo-sidebar.component';
 import { EvoSidebarService } from './evo-sidebar.service';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
+import { EvoIconModule } from '../evo-icon';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    EvoUiKitModule,
-  ],
-  declarations: [ EvoSidebarComponent ],
-  exports: [ EvoSidebarComponent ],
+    imports: [
+        CommonModule,
+        EvoUiKitModule,
+        EvoIconModule,
+    ],
+    declarations: [EvoSidebarComponent],
+    exports: [EvoSidebarComponent],
 })
 export class EvoSidebarModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: EvoSidebarModule,
-      providers: [
-        EvoSidebarService,
-      ],
-    };
-  }
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: EvoSidebarModule,
+            providers: [
+                EvoSidebarService,
+            ],
+        };
+    }
 }
