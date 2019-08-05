e9af0fb141ad9a0516e6b68e5ce42eadab99f950
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-08-05 19:29:59 +0700

    docs(evo-modal): add stories

--- src/stories/components/evo-modal-wrapper/evo-modal-wrapper.component.ts
+++ src/stories/components/evo-modal-wrapper/evo-modal-wrapper.component.ts
@@ -1,21 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { EvoModalService } from '@evo/ui-kit';
+import { catchError } from 'rxjs/operators';
+import { of, timer } from 'rxjs';
 
 @Component({
-  selector: 'app-evo-modal-wrapper',
-  templateUrl: './evo-modal-wrapper.component.html',
-  styleUrls: [ './evo-modal-wrapper.component.scss' ],
+    selector: 'app-evo-modal-wrapper',
+    templateUrl: './evo-modal-wrapper.component.html',
+    styleUrls: ['./evo-modal-wrapper.component.scss'],
 })
 export class EvoModalWrapperComponent {
 
-  @Input() id;
+    constructor(
+        private evoModalService: EvoModalService,
+    ) {
+    }
 
-  constructor(
-    private evoModalService: EvoModalService,
-  ) {}
+    openModal(id) {
+        this.evoModalService.open(id);
+    }
 
-  onClick() {
-    this.evoModalService.open(this.id);
-  }
+    asyncAction = () => {
+        return timer(2000).pipe(
+            catchError(() => {
+                return of(null);
+            }),
+        );
+    }
 
 }
--- src/stories/components/evo-modal.stories.ts
+++ src/stories/components/evo-modal.stories.ts
@@ -1,12 +1,15 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { EvoModalWrapperComponent } from './evo-modal-wrapper/evo-modal-wrapper.component';
 import { EvoModalService, EvoModalModule, EvoButtonModule } from '@evo/ui-kit';
+import { EvoIconModule } from '../../../projects/evo-ui-kit/src/lib/components/evo-icon';
+import { icons } from '../../../projects/evo-ui-kit/icons';
 
 storiesOf('Components/Modal', module)
     .addDecorator(moduleMetadata({
         imports: [
             EvoModalModule,
             EvoButtonModule,
+            EvoIconModule.forRoot([...icons]),
         ],
         providers: [
             EvoModalService,
