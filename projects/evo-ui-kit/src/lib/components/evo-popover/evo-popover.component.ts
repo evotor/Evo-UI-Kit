ba4cc969d0514a61ce58805db00bea63c92aa5cd
Vadim Petrov <pv@petrovvadim.com>
2019-08-12 19:37:04 +0300

    feat(popover): wip rework

--- projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.ts
+++ projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.ts
@@ -1,62 +1,101 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, HostListener, Input, ElementRef, NgZone, AfterViewInit, OnChanges, OnDestroy, SimpleChanges, ViewChild } from '@angular/core';
+import Popper from 'popper.js';
+
+export type EvoPopoverPosition = 'center' | Popper.Placement;
 
 @Component({
     selector: 'evo-popover',
     templateUrl: 'evo-popover.component.html',
-    styleUrls: [ 'evo-popover.component.scss' ],
+    styleUrls: ['evo-popover.component.scss'],
 })
-export class EvoPopoverComponent {
+export class EvoPopoverComponent implements AfterViewInit, OnChanges, OnDestroy {
 
-    /**
-     * Расположение стрелки на media-tablet разрешении
-     */
-        // tslint:disable-next-line
-    @Input('media-tablet-position')
-    mediaTabletPosition: 'right' | 'left' | 'center' = 'center';
+    // TODO: This prop only for support old API. Remove later
+    // tslint:disable-next-line
+    @Input('media-tablet-position') mediaTabletPosition: 'right' | 'left' | 'center' = 'center';
 
-    /**
-     * Расположение стрелки
-     */
-    @Input('position')
-    position: 'right' | 'left' | 'center' = 'center';
-
-    hovered = false;
-
-    private popoverVisibilityTimeout = false;
-
-    @HostListener('mouseenter')
-    onEnter() {
-        this.show();
+    @Input('position') set position(position: EvoPopoverPosition) {
+        this.placement = position === 'center' ? (this.positionMap[position] as Popper.Placement) : position;
     }
 
-    @HostListener('touchend')
-    onTouchEnd() {
-        this.show();
+    // @Input() show = true;
+    @Input() positionFixed = false;
+    @Input() eventsEnabled = true;
+    @Input() modifiers: Popper.Modifiers;
+    @Input() target: string | Element;
+    @ViewChild('popoverWrap') popoverWrap: ElementRef;
+
+    private popper: Popper;
+    private placement: Popper.Placement = 'bottom';
+    // Old API Map
+    private positionMap = {
+        'center': 'bottom',
+    };
+
+    constructor(
+        private el: ElementRef,
+        private zone: NgZone,
+    ) { }
+
+    ngAfterViewInit() {
+        console.log(this.el);
+
+        this.create();
     }
 
-    show(): void {
-        this.popoverVisibilityTimeout = false;
-        this.hovered = true;
+    ngOnDestroy() {
+        this.destroy();
     }
 
-    @HostListener('mouseleave')
-    onLeave() {
-        this.popoverVisibilityTimeout = true;
-        setTimeout(() => {
-            if (this.popoverVisibilityTimeout) {
-                this.hovered = false;
+    ngOnChanges(changes: SimpleChanges) {
+        if (
+            changes.target && !changes.target.firstChange ||
+            changes.position && !changes.position.firstChange ||
+            changes.positionFixed && !changes.positionFixed.firstChange ||
+            changes.eventsEnabled && !changes.eventsEnabled.firstChange
+        ) {
+            this.destroy();
+            this.create();
+        }
+    }
+
+    create() {
+        this.zone.runOutsideAngular(() => {
+            const { placement, positionFixed, eventsEnabled, modifiers } = this;
+
+            this.popper = new Popper(
+                this.getTargetNode(),
+                this.popoverWrap.nativeElement,
+                {
+                    placement,
+                    positionFixed,
+                    eventsEnabled,
+                    modifiers
+                }
+            );
+        });
+    }
+
+    destroy() {
+        if (this.popper) {
+            this.zone.runOutsideAngular(() => {
+                this.popper.destroy();
+            });
+
+            this.popper = null;
+        }
+    }
+
+    private getTargetNode(): Element {
+        if (this.target) {
+            if (typeof this.target === 'string') {
+                return document.querySelector(this.target);
+            } else {
+                return this.target;
             }
-        }, 100);
+        } else {
+            return this.el.nativeElement;
+        }
     }
 
-    getPositionClass(): { [ key: string ]: boolean } {
-        const classes = {};
-        classes[ 'media-tablet-' + this.mediaTabletPosition ] = true;
-        classes[ 'position-' + this.position ] = true;
-        return classes;
-    }
-
-    onClickOutside(event: any): void {
-        this.hovered = false;
-    }
 }
