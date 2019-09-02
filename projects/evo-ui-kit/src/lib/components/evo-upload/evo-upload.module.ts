6adc0da48af397f8b4777992c273e70c31771679
rsboarder <rsboarder@gmail.com>
2019-09-02 17:14:47 +0300

    fix(evo-upload): explicitly set module path

--- projects/evo-ui-kit/src/lib/components/evo-upload/evo-upload.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-upload/evo-upload.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { EvoUploadComponent } from './evo-upload.component';
-import { EvoButtonModule } from '../evo-button';
+import { EvoButtonModule } from '../evo-button/evo-button.module';
 import { CommonModule } from '@angular/common';
-import { EvoIconModule } from '../evo-icon';
+import { EvoIconModule } from '../evo-icon/evo-icon.module';
 import { iconClose } from '@evo/ui-kit/icons/header';
 import { iconDecline, iconFile, iconUpload } from '@evo/ui-kit/icons/system';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
 import { EvoPipesModule } from '../../pipes/evo-pipes.module';
-import { EvoAlertModule } from '../evo-alert';
+import { EvoAlertModule } from '../evo-alert/evo-alert.module';
 
 @NgModule({
     imports: [
