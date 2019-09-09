a5a1f1aa651a7bb4cf66a75cd7e5cff42b971a0c
emazyrina <e.mazyrina@evotor.ru>
2019-09-09 13:31:51 +0300

    feat(evo-tabs): create evo-tab-content component

---
+++ projects/evo-ui-kit/src/lib/components/evo-tabs/evo-tab-content/evo-tab-content.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'evo-tab-content',
+    templateUrl: './evo-tab-content.component.html',
+    styleUrls: ['./evo-tab-content.component.css']
+})
+export class EvoTabContentComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
