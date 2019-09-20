9a00b069f9fec06ac72672c956588e33ef753a8b
emazyrina <e.mazyrina@evotor.ru>
2019-09-20 15:49:58 +0300

    fix(evo-tabs): add new exports to public-api

--- projects/evo-ui-kit/src/lib/components/evo-tabs/public-api.ts
+++ projects/evo-ui-kit/src/lib/components/evo-tabs/public-api.ts
@@ -1,3 +1,5 @@
 export * from './evo-tabs.module';
 export * from './evo-tabs.component';
 export * from './evo-tab/evo-tab.component';
+export * from './evo-tab-content/evo-tab-content.component';
+export * from './evo-tabs.service';
