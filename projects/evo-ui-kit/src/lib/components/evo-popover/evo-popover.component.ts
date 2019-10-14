ea685539b3effb6254876122a1e8c33692ebf5cb
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-10-14 17:43:08 +0700

    refactor(evo-popover): refactor, codestyle

--- projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.ts
+++ projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.ts
@@ -1,8 +1,21 @@
-import { Component, HostListener, Input, ElementRef, NgZone, AfterViewInit, OnChanges, OnDestroy, SimpleChanges, ViewChild } from '@angular/core';
+import {
+    Component,
+    HostListener,
+    Input,
+    ElementRef,
+    NgZone,
+    AfterViewInit,
+    OnChanges,
+    OnDestroy,
+    SimpleChanges,
+    ViewChild
+} from '@angular/core';
 import Popper from 'popper.js';
 
-const DEFAULT_DELAY_SHOW = 0;
-const DEFAULT_DELAY_HIDE = 100;
+const DEFAULT_DELAY = {
+    show: 0,
+    hide: 100,
+};
 
 export type EvoPopoverPosition = 'center' | Popper.Placement;
 
@@ -14,28 +27,45 @@ export interface EvoPopoverDelay {
 @Component({
     selector: 'evo-popover',
     templateUrl: 'evo-popover.component.html',
-    styleUrls: [ 'evo-popover.component.scss' ],
+    styleUrls: ['evo-popover.component.scss'],
 })
-export class EvoPopoverComponent {
+export class EvoPopoverComponent implements AfterViewInit, OnChanges, OnDestroy {
 
-    /**
-     * Расположение стрелки на media-tablet разрешении
-     */
-        // tslint:disable-next-line
-    @Input('media-tablet-position')
-    mediaTabletPosition: 'right' | 'left' | 'center' = 'center';
+    // TODO: This prop only for support old API. Remove later
+    // tslint:disable-next-line
+    @Input('media-tablet-position') mediaTabletPosition: 'right' | 'left' | 'center' = 'center';
 
-    /**
-     * Расположение стрелки
-     */
-    @Input('position')
-    position: 'right' | 'left' | 'center' = 'center';
+    @Input('position') set position(position: EvoPopoverPosition) {
+        this.placement = position === 'center' ? (this.positionMap[position] as Popper.Placement) : position;
+    }
 
     @Input() show = false;
     @Input() positionFixed = false;
     @Input() eventsEnabled = true;
     @Input() modifiers: Popper.Modifiers;
     @Input() target: string | Element;
+
+    @Input('delay') set setDelay(value: any) {
+        if (typeof value === 'number' && value >= 0) {
+            this.delay = {
+                show: value,
+                hide: value,
+            };
+            return;
+        }
+
+        if (typeof value === 'object') {
+            this.delay = {
+                show: value.show >= 0 ? value.show : DEFAULT_DELAY.show,
+                hide: value.hide >= 0 ? value.hide : DEFAULT_DELAY.hide,
+            };
+            return;
+        }
+
+        this.delay.show = DEFAULT_DELAY.show;
+        this.delay.hide = DEFAULT_DELAY.hide;
+    }
+
     @ViewChild('popover', {static: false}) el: ElementRef;
     @ViewChild('popoverWrap', {static: false}) popoverWrap: ElementRef;
 
@@ -45,8 +75,81 @@ export class EvoPopoverComponent {
     private visibilityTimeout = null;
 
     // Old API Map
-    private positionMap = { 'center': 'bottom' };
-    private popoverVisibilityTimeout = false;
+    private positionMap = {'center': 'bottom'};
+    private arrowSize = 16;
+    private borderRadius = 6;
+    private defaultModifiers = {
+        offset: {
+            // Modifier for Arrow offset
+            fn: data => {
+                const {offsets, placement, arrowStyles} = data;
+                const {reference, popper} = offsets;
+                if (placement === 'bottom-start' && ((reference.width / 2) + this.arrowSize) > popper.width) {
+                    arrowStyles.left = popper.width - this.arrowSize - this.borderRadius + 'px';
+                }
+                if (placement === 'bottom-end' && ((reference.width / 2) + this.arrowSize) > popper.width) {
+                    arrowStyles.left = this.borderRadius + 'px';
+                }
+                return data;
+            }
+        }
+    };
+
+    constructor(
+        private zone: NgZone,
+    ) {
+    }
+
+    ngAfterViewInit() {
+        this.create();
+    }
+
+    ngOnDestroy() {
+        this.destroy();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        const {target, position, positionFixed, eventsEnabled} = changes;
+        if (
+            target && !target.firstChange ||
+            position && !position.firstChange ||
+            positionFixed && !positionFixed.firstChange ||
+            eventsEnabled && !eventsEnabled.firstChange
+        ) {
+            this.destroy();
+            this.create();
+        }
+    }
+
+    create() {
+        this.zone.runOutsideAngular(() => {
+            const {placement, positionFixed, eventsEnabled, modifiers} = this;
+
+            this.popper = new Popper(
+                this.getTargetNode(),
+                this.popoverWrap.nativeElement,
+                {
+                    placement,
+                    positionFixed,
+                    eventsEnabled,
+                    modifiers: {
+                        ...this.defaultModifiers,
+                        ...modifiers,
+                    },
+                }
+            );
+        });
+    }
+
+    destroy() {
+        if (this.popper) {
+            this.zone.runOutsideAngular(() => {
+                this.popper.destroy();
+            });
+
+            this.popper = null;
+        }
+    }
 
     @HostListener('mouseenter')
     onEnter() {
@@ -85,15 +188,35 @@ export class EvoPopoverComponent {
 
     private togglePopover(show: boolean = false) {
         const action = show ? 'show' : 'hide';
+        const delayValue = this.delay && this.delay[action] > 0 ? this.delay[action] : 0;
+        const toggle = () => {
+            if (show) {
+                this.showPopover();
+            } else {
+                this.hidePopover();
+            }
+        };
         if (this.visibilityTimeout) {
             clearTimeout(this.visibilityTimeout);
         }
-        if (this.delay && this.delay[action] > 0) {
+        if (delayValue > 0) {
             this.visibilityTimeout = setTimeout(() => {
-                show ? this.showPopover() : this.hidePopover();
-            }, this.delay[action]);
+                toggle();
+            }, delayValue);
         } else {
-            show ? this.showPopover() : this.hidePopover();
+            toggle();
         }
     }
+
+    private getTargetNode(): Element {
+        if (!this.target) {
+            return this.el.nativeElement;
+        }
+        if (typeof this.target === 'string') {
+            return document.querySelector(this.target);
+        } else {
+            return this.target;
+        }
+    }
+
 }
