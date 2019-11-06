5a271e9831f68d3ab92f58ad516911cb76925586
Andrey Matveev <andrey.g.matveev@gmail.com>
2019-11-06 16:11:06 +0700

    feat(evo-toast): use evo-icon for close, set colors for links

--- projects/evo-ui-kit/src/lib/components/evo-toast/evo-toast.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-toast/evo-toast.module.ts
@@ -1,15 +1,25 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EvoToastComponent } from './evo-toast.component';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
+import { EvoIconModule } from '../evo-icon';
+import { iconClose } from '@evo/ui-kit/icons/header';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    EvoUiKitModule,
-  ],
-  declarations: [ EvoToastComponent ],
-  exports: [ EvoToastComponent ],
+    imports: [
+        CommonModule,
+        EvoUiKitModule,
+        EvoIconModule.forRoot([
+            {
+                name: 'sidebarIcons',
+                shapes: {
+                    'close': iconClose,
+                },
+            }
+        ]),
+    ],
+    declarations: [EvoToastComponent],
+    exports: [EvoToastComponent],
 })
-export class EvoToastModule {}
+export class EvoToastModule {
+}
