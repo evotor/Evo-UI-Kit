5fd1c779dd1bf2601cc8669b5a54f53738cda560
Vadim Petrov <pv@petrovvadim.com>
2020-02-10 18:45:48 +0300

    feat(popover): +specs

--- projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.spec.ts
+++ projects/evo-ui-kit/src/lib/components/evo-popover/evo-popover.component.spec.ts
@@ -3,12 +3,12 @@ import { EvoPopoverComponent } from './index';
 import { Component, ViewChild } from '@angular/core';
 import { EvoUiClassDirective } from '../../directives/';
 
-const position = 'right';
+const initialPosition = 'right';
 
 @Component({
     selector: 'evo-host-component',
     template: `
-        <evo-popover position="${position}">
+        <evo-popover [position]="position">
             <p style="max-width: 360px">Some text content...<br>
                 <a href="https://evotor.ru" target="_blank">Some link</a>
             </p>
@@ -18,13 +18,14 @@ const position = 'right';
         </evo-popover>`,
 })
 class TestHostComponent {
-    @ViewChild(EvoPopoverComponent)
-    public popoverComponent: EvoPopoverComponent;
+    @ViewChild(EvoPopoverComponent) popoverComponent: EvoPopoverComponent;
+    position = initialPosition;
 }
 
 describe('EvoPopoverComponent', () => {
     let testHostComponent: TestHostComponent;
     let testHostFixture: ComponentFixture<TestHostComponent>;
+    let popoverComponent: EvoPopoverComponent;
     let tipEl: HTMLElement;
 
     beforeEach(async(() => {
@@ -40,6 +41,7 @@ describe('EvoPopoverComponent', () => {
     beforeEach(() => {
         testHostFixture = TestBed.createComponent(TestHostComponent);
         testHostComponent = testHostFixture.componentInstance;
+        popoverComponent = testHostComponent.popoverComponent;
         tipEl = testHostFixture.nativeElement.querySelector('.evo-popover__tip');
     });
 
@@ -61,10 +63,26 @@ describe('EvoPopoverComponent', () => {
         expect(tipEl.classList.contains('evo-popover__tip_visible')).toBeFalsy();
     }));
 
-    it(`should have attribute '[x-placement="${position}"]'`, () => {
+    it(`should have attribute '[data-popper-placement="${initialPosition}"]'`, fakeAsync(() => {
         testHostComponent.popoverComponent.onEnter();
         testHostFixture.detectChanges();
-        expect(testHostFixture.nativeElement.querySelector('.evo-popover__tip').getAttribute('x-placement'))
-            .toEqual(position);
-    });
+        tick(100);
+        expect(popoverComponent['placement']).toEqual(initialPosition);
+        expect(testHostFixture.nativeElement.querySelector('.evo-popover__tip').getAttribute('data-popper-placement'))
+            .toEqual(initialPosition);
+    }));
+
+    it(`should unsubscribe from updates after destroy'`, fakeAsync(() => {
+        popoverComponent.ngOnDestroy();
+        const subscriptions$ = popoverComponent['subscriptions$'];
+        expect(subscriptions$.observers.length).toEqual(0);
+        expect(subscriptions$.isStopped).toEqual(true);
+    }));
+
+    it(`should create new instance after position update'`, fakeAsync(() => {
+        const firstInstance = popoverComponent['popper'];
+        testHostComponent.position = 'bottom-end';
+        testHostFixture.detectChanges();
+        expect(firstInstance === popoverComponent['popper']).toBeFalsy();
+    }));
 });
