cd77737bb37bb2af9fdbe3e2fa491e8b1e629168
Andrey Matveev <andrey.g.matveev@gmail.com>
2020-04-29 11:37:05 +0700

    test(project): fix code style

--- projects/evo-ui-kit/src/test.ts
+++ projects/evo-ui-kit/src/test.ts
@@ -24,5 +24,5 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context.keys().forEach(context);
 /* tslint:enable */
