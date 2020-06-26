5cafc2cb6a0b86983023964deb7c860f406a57cb
Andrey Matveev <andrey.g.matveev@gmail.com>
2020-06-26 12:54:19 +0700

    fix(evo-datepicker): remove doubling options of FlatpickrOptions

--- projects/evo-ui-kit/src/lib/components/evo-datepicker/flatpickr-options.interface.ts
+++ projects/evo-ui-kit/src/lib/components/evo-datepicker/flatpickr-options.interface.ts
@@ -1,33 +1,12 @@
 import { BaseOptions } from 'flatpickr/dist/types/options';
 
 export interface FlatpickrOptions extends Partial<BaseOptions> {
-    altFormat?: string;
-    altInput?: boolean;
-    altInputClass?: string;
-    allowInput?: boolean;
-    appendTo?: HTMLElement;
-    clickOpens?: boolean;
-    dateFormat?: string;
     defaultDate?: Date | Date[];
     disable?: Array<string | Date>;
-    disableMobile?: boolean;
     enable?: Array<string | Date>;
-    enableTime?: boolean;
-    enableSeconds?: boolean;
-    hourIncrement?: number;
-    inline?: boolean;
     maxDate?: string | Date;
     minDate?: string | Date;
-    minuteIncrement?: number;
-    nextArrow?: string;
-    noCalendar?: boolean;
-    prevArrow?: string;
-    shorthandCurrentMonth?: boolean;
-    static?: boolean;
-    time_24hr?: boolean;
     utc?: boolean;
-    weekNumbers?: boolean;
-    wrap?: boolean;
 
     // Needed so we can access options[key].
     [key: string]: any;
