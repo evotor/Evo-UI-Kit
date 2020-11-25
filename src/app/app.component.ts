3b7388ef1db5cf9c038afad4743c65de54afba4a
Bayyr Oorjak <the.bayyr.oorjak@gmail.com>
2020-11-25 17:16:15 +0700

    feat(table): add dynamic columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     form = new FormBuilder().group({
         'test': ['', [Validators.required]],
     });
+
+    columns = [
+        { prop: 'id', show: true },
+        { prop: 'name', show: true },
+        { prop: 'sirname', show: true },
+    ];
+
+    visibleColumns = [];
+
+    data = [
+        { id: 1, name: 'Name1', sirname: 'Sirname1' },
+        { id: 2, name: 'Name2', sirname: 'Sirname2' },
+        { id: 3, name: 'Name3', sirname: 'Sirname3' },
+    ];
+
+    ngOnInit() {
+        this.updateVisibleColumns();
+    }
+
+    updateVisibleColumns() {
+        this.visibleColumns = this.columns.filter(col => col.show).map(col => col.prop);
+    }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EvoUiKitModule } from '../../projects/evo-ui-kit/src/lib/evo-ui-kit.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EvoInputModule } from '../../projects/evo-ui-kit/src/lib/components/evo-input';
-import { EvoChipModule } from '../../projects/evo-ui-kit/src/lib/components/evo-chip';
+import { EvoTableModule } from 'projects/evo-ui-kit/src/lib/components/evo-table';
 
 @NgModule({
     declarations: [
@@ -16,6 +16,8 @@ import { EvoChipModule } from '../../projects/evo-ui-kit/src/lib/components/evo-
         EvoUiKitModule,
         EvoInputModule,
         ReactiveFormsModule,
+        EvoTableModule,
+        FormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
