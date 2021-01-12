5c455ed49519ae6b9ebd1e867fb75f39b4d5bd05
Andrey Matveev <andrey.g.matveev@gmail.com>
2021-01-12 12:43:42 +0700

    docs(app): clean app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EvoUiKitModule } from '../../projects/evo-ui-kit/src/lib/evo-ui-kit.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { EvoInputModule, EvoControlLabelModule, EvoButtonModule } from '@evo/ui-kit';
+// import { EvoIconButtonModule } from '../../projects/evo-ui-kit/src/lib/components/evo-icon-button';
 
 @NgModule({
     declarations: [
@@ -13,10 +12,6 @@ import { EvoInputModule, EvoControlLabelModule, EvoButtonModule } from '@evo/ui-
     ],
     imports: [
         BrowserModule,
-        EvoUiKitModule,
-        EvoButtonModule,
-        EvoControlLabelModule,
-        EvoInputModule,
         ReactiveFormsModule,
     ],
     providers: [],
