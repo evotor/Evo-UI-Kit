cab08a0feca1af92e376c388aa85456f94fa9ece
Andrey Fastovskiy <a.fastovskiy@evotor.ru>
2021-01-14 13:28:33 +0300

    fix(evo-icon-button): overriding icons with a spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-// import { EvoIconButtonModule } from '../../projects/evo-ui-kit/src/lib/components/evo-icon-button';
-
 @NgModule({
     declarations: [
         AppComponent,
