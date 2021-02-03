b110c92ef9e620cc2e13aa3ecef9f6cda07fd8ce
Andrey Fastovskiy <a.fastovskiy@evotor.ru>
2021-02-03 18:57:56 +0300

    chore(evo-accordion-content): create empty component

---
+++ projects/evo-ui-kit/src/lib/components/evo-accordion/evo-accordion-content/evo-accordion-content.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'evo-accordion-content',
+    templateUrl: './evo-accordion-content.component.html',
+    styleUrls: ['./evo-accordion-content.component.scss']
+})
+export class EvoAccordionContentComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
--- projects/evo-ui-kit/src/lib/components/evo-accordion/public-api.ts
+++ projects/evo-ui-kit/src/lib/components/evo-accordion/public-api.ts
@@ -2,3 +2,4 @@ export * from './evo-accordion.module';
 export * from './evo-accordion.component';
 export * from './evo-accordion-title/evo-accordion-title.component';
 export * from './evo-accordion-panel/evo-accordion-panel.component';
+export * from './evo-accordion-content/evo-accordion-content.component';
