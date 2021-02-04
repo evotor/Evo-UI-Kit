b8d149851c2b4d5179f5821a01d91ce5e18a7e3e
Andrey Fastovskiy <a.fastovskiy@evotor.ru>
2021-02-04 14:24:52 +0300

    fix(evo-accordion-panel): selector and styles

--- projects/evo-ui-kit/src/lib/components/evo-accordion/evo-accordion-panel/evo-accordion-panel.component.ts
+++ projects/evo-ui-kit/src/lib/components/evo-accordion/evo-accordion-panel/evo-accordion-panel.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EvoIfExpandedService } from '../../../services/evo-if-expanded.service';
 
 @Component({
-    selector: 'evo-evo-accordion-panel',
+    selector: 'evo-accordion-panel',
     template: `<ng-content></ng-content>`,
     styleUrls: ['./evo-accordion-panel.component.scss'],
     providers: [EvoIfExpandedService]
