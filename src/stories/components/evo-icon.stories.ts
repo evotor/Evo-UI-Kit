708df1042cd9e3610ba13751881c0eef36f83fa1
Andrey Matveev <andrey.g.matveev@gmail.com>
2021-02-08 11:54:36 +0700

    docs(evo-icon): add assets info

--- src/stories/components/evo-icon.stories.ts
+++ src/stories/components/evo-icon.stories.ts
@@ -114,16 +114,43 @@ imports: [
     .add('Big color icon assets', () => ({
         template: `
 <style>
-.wrapper {display:flex;flex-flow: row wrap;}
+.wrapper {
+    display:flex;
+    flex-flow: row wrap;
+}
 .icon {
     width: 100px;
     margin: 32px;
 }
+pre {
+    padding: 20px;
+    margin-top: 24px;
+    background: #eee;
+    border-radius: 4px;
+}
+img {
+    display: block;
+    width: 48px;
+    height: 48px;
+}
 </style>
-<div class="wrapper">
-    <div *ngFor="let iconName of icons" class="icon">
-        <img src="/assets/color-icons/{{iconName}}" style="width: 48px; height: 48px; display: block;"/>
-        <p>{{ iconName }}</p>
+<div class="evo-content">
+    <h3 class="evo-title evo-title_h3">Import assets in your project</h3>
+    <pre>
+    angular.json
+
+    {{ '{' }}
+        "glob": "**/*",
+        "input": "./node_modules/@evo/ui-kit/assets/",
+        "output": "./assets/ui-kit/"
+    {{ '}' }}
+    </pre>
+
+    <div class="wrapper">
+        <div *ngFor="let iconName of icons" class="icon">
+            <img src="/assets/color-icons/{{ iconName }}" />
+            <p>{{ iconName }}</p>
+        </div>
     </div>
 </div>
         `,
