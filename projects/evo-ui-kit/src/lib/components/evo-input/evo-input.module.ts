8c2e372cd0579ab14c66a5b4c6aac7f2a9ec062b
Andrey Matveev <andrey.g.matveev@gmail.com>
2021-04-16 15:48:48 +0700

    fix(evo-input): replace icon, update small sizes

--- projects/evo-ui-kit/src/lib/components/evo-input/evo-input.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-input/evo-input.module.ts
@@ -5,16 +5,25 @@ import { EvoInputComponent } from './evo-input.component';
 import { EvoUiKitModule } from '../../evo-ui-kit.module';
 import { EvoControlErrorModule } from '../evo-control-error/evo-control-error.module';
 import { FormsModule } from '@angular/forms';
+import { EvoIconModule } from '../evo-icon';
+import { iconHelp } from '@evo/ui-kit/icons/system';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    EvoUiKitModule,
-    EvoControlErrorModule,
-    IMaskModule,
-  ],
-  declarations: [ EvoInputComponent ],
-  exports: [ EvoInputComponent ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        EvoUiKitModule,
+        EvoControlErrorModule,
+        IMaskModule,
+        EvoIconModule.forRoot([{
+            name: 'icons',
+            shapes: {
+                help: iconHelp,
+            },
+        }]),
+    ],
+    declarations: [EvoInputComponent],
+    exports: [EvoInputComponent],
 })
-export class EvoInputModule {}
+export class EvoInputModule {
+}
