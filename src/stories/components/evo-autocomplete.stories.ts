23523726b29ffb7b8fc38795354667cb29083ed2
Andrey Fastovskiy <a.fastovskiy@evotor.ru>
2021-08-09 16:00:13 +0300

    refactor(evo-autocomplete): correct comment

--- src/stories/components/evo-autocomplete.stories.ts
+++ src/stories/components/evo-autocomplete.stories.ts
@@ -313,7 +313,7 @@ storiesOf('Components/Autocomplete', module)
             </evo-autocomplete>
         </div>
         <div class="story-section">
-            <h3><code>multipleInline</code> with checkboxes and single label (via <code>evo-autocomplete-default-option</code>)</h3>
+            <h3><code>multipleInline</code> with checkboxes and overal label (via <code>evo-autocomplete-default-option</code>)</h3>
             <evo-autocomplete
                 [items]="items"
                 bindLabel="label"
