2b3744f132f4a31d8729c1be3ef5c3349342dffd
dmitriy <d.pimenov>
2021-09-29 10:39:00 +0300

    docs(evo-badge): update stories

--- src/stories/components/evo-badge.stories.ts
+++ src/stories/components/evo-badge.stories.ts
@@ -1,11 +1,12 @@
 import { moduleMetadata, storiesOf } from '@storybook/angular';
-import { BadgeColor, EvoBadgeModule } from '../../../projects/evo-ui-kit/src/lib/components/evo-badge';
+import { EvoBadgeModule } from '../../../projects/evo-ui-kit/src/lib/components/evo-badge';
+import { EVO_COLOR_PALETTE } from '../../../projects/evo-ui-kit/src/lib/common/constants/evo-color-palette';
 
 storiesOf('Components/Badge', module)
     .addDecorator(
         moduleMetadata({
             imports: [
-                EvoBadgeModule
+                EvoBadgeModule,
             ]
         })
     )
@@ -18,17 +19,32 @@ storiesOf('Components/Badge', module)
             }
         </style>
         <div class="story-container">
-            <h2>Badges</h2>
+            <h2>Badges and available colors</h2>
             <div class="story-section">
                 <ng-container *ngFor="let color of colorsList">
                     <div class="badge-container">
-                        <evo-badge color='{{color}}'>{{color}}</evo-badge>
+                        <evo-badge [color]="color">{{color}}</evo-badge>
                     </div>
                 </ng-container>
             </div>
         </div>
         `,
         props: {
-            colorsList: Object.keys(BadgeColor)
+            colorsList: [
+                'DEFAULT_NO_COLOR',
+                EVO_COLOR_PALETTE.SUCCESS,
+                EVO_COLOR_PALETTE.ERROR,
+                EVO_COLOR_PALETTE.ICON_DARK,
+                EVO_COLOR_PALETTE.GRAPH1,
+                EVO_COLOR_PALETTE.GRAPH2,
+                EVO_COLOR_PALETTE.GRAPH3,
+                EVO_COLOR_PALETTE.GRAPH4,
+                EVO_COLOR_PALETTE.GRAPH5,
+                EVO_COLOR_PALETTE.GRAPH6,
+                EVO_COLOR_PALETTE.GRAPH7,
+                EVO_COLOR_PALETTE.GRAPH8,
+                EVO_COLOR_PALETTE.GRAPH9,
+                EVO_COLOR_PALETTE.GRAPH10,
+            ]
         }
     }));
