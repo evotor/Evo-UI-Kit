8c3b17f9a5a395a765efd76311c7f03796a220d4
Andrey Matveev <andrey.g.matveev@gmail.com>
2021-10-09 12:43:53 +0700

    ci(notify): release notify workflow

--- .github/scripts/notify-telegram.sh
+++ .github/scripts/notify-telegram.sh
@@ -2,9 +2,12 @@
 
 TIME="10"
 URL="https://api.telegram.org/bot$FRONTEND_TELEGRAM_BOT/sendMessage"
-MESSAGE="$1%0A%0AProject: $CI_PROJECT_NAME"
+MESSAGE="$1%0A%0AProject: $PROJECT_NAME"
+
+echo $URL
+echo $MESSAGE
 
 curl -s --max-time $TIME\
  --data "chat_id=$FRONTEND_TELEGRAM_CHAT"\
  --data "text=$MESSAGE"\
- $URL > /dev/null
+ $URL # > /dev/null
