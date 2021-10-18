ab1c45c561d8bfa96e77a4fa09bb34fc4e86297f
Andrey Matveev andrey.g.matveev@gmail.com <a.matveev@evotor.ru>
2021-10-18 11:36:37 +0700

    feat(typings): simplify EvoColor type + rename EVO_COLOR

--- src/stories/components/evo-badge.stories.ts
+++ src/stories/components/evo-badge.stories.ts
@@ -1,6 +1,6 @@
 import { moduleMetadata, storiesOf } from '@storybook/angular';
 import { EvoBadgeModule } from '../../../projects/evo-ui-kit/src/lib/components/evo-badge';
-import { EVO_COLOR_PALETTE } from '../../../projects/evo-ui-kit/src/lib/common/constants/evo-color-palette';
+import { EVO_COLOR } from '../../../projects/evo-ui-kit/src/lib/common/enums/index';
 
 storiesOf('Components/Badge', module)
     .addDecorator(
@@ -32,19 +32,19 @@ storiesOf('Components/Badge', module)
         props: {
             colorsList: [
                 'DEFAULT_NO_COLOR',
-                EVO_COLOR_PALETTE.SUCCESS,
-                EVO_COLOR_PALETTE.ERROR,
-                EVO_COLOR_PALETTE.ICON_DARK,
-                EVO_COLOR_PALETTE.GRAPH1,
-                EVO_COLOR_PALETTE.GRAPH2,
-                EVO_COLOR_PALETTE.GRAPH3,
-                EVO_COLOR_PALETTE.GRAPH4,
-                EVO_COLOR_PALETTE.GRAPH5,
-                EVO_COLOR_PALETTE.GRAPH6,
-                EVO_COLOR_PALETTE.GRAPH7,
-                EVO_COLOR_PALETTE.GRAPH8,
-                EVO_COLOR_PALETTE.GRAPH9,
-                EVO_COLOR_PALETTE.GRAPH10,
+                EVO_COLOR.success,
+                EVO_COLOR.error,
+                EVO_COLOR.iconDark,
+                EVO_COLOR.graph1,
+                EVO_COLOR.graph2,
+                EVO_COLOR.graph3,
+                EVO_COLOR.graph4,
+                EVO_COLOR.graph5,
+                EVO_COLOR.graph6,
+                EVO_COLOR.graph7,
+                EVO_COLOR.graph8,
+                EVO_COLOR.graph9,
+                EVO_COLOR.graph10,
             ]
         }
     }));
