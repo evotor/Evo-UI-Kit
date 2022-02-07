ae8a4b037f3e7b8adae505f7c4bbde6bd7d36401
andreymatveev <a.matveev@evotor.ru>
2022-02-07 15:33:41 +0700

    feat(evo-button): app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EvoButtonModule } from '../../projects/evo-ui-kit/src/lib/components/evo-button';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EvoIconModule } from '../../projects/evo-ui-kit/src/lib/components/evo-icon';
+import { icons } from '../../projects/evo-ui-kit/icons';
 
 @NgModule({
     declarations: [
         AppComponent,
     ],
     imports: [
+        EvoButtonModule,
         BrowserModule,
         ReactiveFormsModule,
+        EvoIconModule.forRoot([...icons]),
     ],
     providers: [],
     bootstrap: [AppComponent],
