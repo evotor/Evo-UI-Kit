780526edfb62e287956ad9ace5737606f59d2150
andreymatveev <andrey.g.matveev@gmail.com>
2022-02-21 12:30:51 +0700

    feat(evo-loader): add evo-circular-loader
    (cherry picked from commit fcbd6b81a5993f843125a74c62556ae46f68f078)

---
+++ projects/evo-ui-kit/src/lib/components/evo-loader/evo-circular-loader.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'evo-circular-loader',
+    templateUrl: './evo-circular-loader.component.html',
+    styleUrls: ['./evo-circular-loader.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EvoCircularLoaderComponent {
+}
