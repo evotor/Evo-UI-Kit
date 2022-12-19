806507d3783a4ad796984f8d547a024e3e36e49f
a.alifanova <a.alifanova@evotor.ru>
2022-12-19 15:46:08 +0300

    fix(evo-note): clear code

--- projects/evo-ui-kit/src/lib/components/evo-note/evo-note.component.ts
+++ projects/evo-ui-kit/src/lib/components/evo-note/evo-note.component.ts
@@ -1,13 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { EvoNoteType } from './types/evo-note-type';
 
-const iconsMap = new Map<EvoNoteType, string>([
-    ['danger', 'Alert Circle.svg'],
-    ['info', 'Info Circle.svg'],
-    ['success', 'OK.svg'],
-    ['warning', 'Alert Triangle.svg'],
-]);
-
 @Component({
     selector: 'evo-note',
     templateUrl: './evo-note.component.html',
@@ -16,7 +9,6 @@ const iconsMap = new Map<EvoNoteType, string>([
 })
 export class EvoNoteComponent {
     @Input() closable = false;
-    @Input() hideIcon = false;
     @Input() iconSrc: string;
     @Input() type: EvoNoteType = 'success';
 
@@ -38,17 +30,5 @@ export class EvoNoteComponent {
 
     onCloseClick(): void {
         this.close.emit();
-    }
-
-    getIconSrc(): string {
-        if (this.iconSrc) {
-            return this.iconSrc;
-        }
-
-        if (!this.hideIcon) {
-            return `../../../svg/color-icons/${iconsMap.get(this.type)}`;
-        }
-
-        return undefined;
     }
 }
--- src/stories/components/evo-note.stories.ts
+++ src/stories/components/evo-note.stories.ts
@@ -13,29 +13,21 @@ storiesOf('Components/Note', module)
     template: `
         <evo-note>Содержимое уведомления типа <strong>Успех (success, по умолчанию)</strong></evo-note>  <br/>
         <evo-note type="warning">Содержимое уведомления типа <strong>Предупреждение (warning)</strong></evo-note> <br/>
-        <evo-note type="danger">Содержимое уведолмения типа <strong>Ошибка (danger)</strong></evo-note> <br/>
-        <evo-note type="info">Содержимое уведолмения типа <strong>Инфо (info)</strong></evo-note>
+        <evo-note type="danger">Содержимое уведомления типа <strong>Ошибка (danger)</strong></evo-note> <br/>
+        <evo-note type="info">Содержимое уведомления типа <strong>Инфо (info)</strong></evo-note>
         `,
 }))
-    .add('without icon', () => ({
-        template: `
-        <evo-note [hideIcon]="true">Содержимое уведомления типа <strong>Успех (success, по умолчанию)</strong></evo-note> <br/>
-        <evo-note [hideIcon]="true" type="warning">Содержимое уведомления типа <strong>Предупреждение (warning)</strong></evo-note> <br/>
-        <evo-note [hideIcon]="true" type="danger">Содержимое уведолмения типа <strong>Ошибка (danger)</strong></evo-note> <br/>
-        <evo-note [hideIcon]="true" type="info">Содержимое уведолмения типа <strong>Инфо (info)</strong></evo-note>
-        `
-    }))
     .add('with custom icon URL', () => ({
         template: `
         <evo-note iconSrc="https://market-test.evotor.ru/static/targeting/4512390b-98e6-4b6d-8c08-7f4d3beab53c.png">
-       Будьте внимательны при создании тарифных планов. После создания и отправки на модерацию, вы уже не сможете изменять.
+           Для отмены подписки или разовой оплаты, перейдите в раздел «Мои приложения» и удалите нужное приложение
         </evo-note>
         `
     }))
     .add('with close icon', () => ({
         template: `
-            <evo-note [closable]="true" (close)="alertCloseAction()">Уведомление с иконкой «Закрыть»</evo-note>`,
+            <evo-note [closable]="true" (close)="evoNoteCloseAction()">Уведомление с иконкой «Закрыть»</evo-note>`,
         props: {
-            alertCloseAction: action('evo-alert close action'),
+            alertCloseAction: action('evo-note close action'),
         },
     }));
