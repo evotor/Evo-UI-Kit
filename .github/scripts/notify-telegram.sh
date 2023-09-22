eae44dbf1bbf14c3d5c96dfe3abd30fc2c0d3a01
Andrey Matveev <andrey.g.matveev@gmail.com>
2023-09-22 22:11:55 +0700

    ci: test action

--- .github/scripts/notify-telegram.sh
+++ .github/scripts/notify-telegram.sh
@@ -4,13 +4,12 @@ TIME="10"
 URL="https://api.telegram.org/bot$FRONTEND_TELEGRAM_BOT/sendMessage"
 MESSAGE="$1%0A%0AProject: $PROJECT_NAME"
 
-# echo $URL
-# echo $MESSAGE
-echo $FRONTEND_REPLY_TO_MESSAGE_ID
+echo $URL
+echo $MESSAGE
 
-# curl -s --max-time $TIME\
-#  --data "chat_id=$FRONTEND_TELEGRAM_CHAT"\
-#  --data "reply_to_message_id=$FRONTEND_REPLY_TO_MESSAGE_ID"\
-#  --data "text=$MESSAGE"\
-#  --data "parse_mode=markdown"\
-#  $URL # > /dev/null
+curl -s --max-time $TIME\
+ --data "chat_id=$FRONTEND_TELEGRAM_CHAT"\
+ --data "reply_to_message_id=$FRONTEND_REPLY_TO_MESSAGE_ID"\
+ --data "text=$MESSAGE"\
+ --data "parse_mode=markdown"\
+ $URL # > /dev/null
