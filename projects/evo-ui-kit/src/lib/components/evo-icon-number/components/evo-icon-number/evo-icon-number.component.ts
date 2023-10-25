921c8913b12ec1f6268c6b8a349136e29a3c5460
andreymatveev <a.matveev@evotor.ru>
2023-10-25 19:29:16 +0700

    feat(evo-icon-number): add new component
    (cherry picked from commit be8ece9e70b94eccc85e7882bf54e3487a45804b)

---
+++ projects/evo-ui-kit/src/lib/components/evo-icon-number/components/evo-icon-number/evo-icon-number.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'evo-icon-number',
+    templateUrl: './evo-icon-number.component.html',
+    styleUrls: ['./evo-icon-number.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EvoIconNumberComponent {
+    @Input() number: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+}
---
+++ projects/evo-ui-kit/src/lib/components/evo-icon-number/evo-icon-number.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EvoIconNumberComponent} from './components/evo-icon-number/evo-icon-number.component';
+
+@NgModule({
+    declarations: [EvoIconNumberComponent],
+    imports: [CommonModule],
+    exports: [EvoIconNumberComponent],
+})
+export class EvoIconNumberModule {}
