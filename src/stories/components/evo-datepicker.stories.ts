c3141162510bb51440bf4a175d099871cf329b0b
andreymatveev <a.matveev@evotor.ru>
2023-11-24 11:44:18 +0300

    fix(evo-datepicker): fix range theme markup
    (cherry picked from commit 3312f833289c23373e526601905c621a3516858b)

--- src/stories/components/evo-datepicker.stories.ts
+++ src/stories/components/evo-datepicker.stories.ts
@@ -181,11 +181,34 @@ export const CSSCustomization = () => ({
             <div style='width: 400px; margin: 20px' [formGroup]="form">
                 <evo-datepicker style='--evo-datepicker-icon-color: red' formControlName="formControlName" [config]="exampleOptions" [maskedInput]="true"></evo-datepicker>
             </div>
+
+            <h3>
+                Custom calendar icon color for range theme
+                <code>--evo-datepicker-range-icon-color: red;</code>
+            </h3>
+
+            <div style='width: 400px; margin: 20px' [formGroup]="form">
+                <evo-datepicker style='--evo-datepicker-range-icon-color: green' formControlName="formControlName" [config]="exampleRangeOptions" theme="range" ></evo-datepicker>
+            </div>
+
+            <h3>
+                Word wrap for range theme
+                <code>--evo-datepicker-range-value-white-space: normal;</code>
+            </h3>
+
+            <p><strong>nowrap</strong> is set by default</p>
+
+            <div style='width: 200px; margin: 20px' [formGroup]="form">
+                <evo-datepicker style='--evo-datepicker-range-value-white-space: normal' formControlName="formControlName" [config]="exampleRangeOptions" theme="range" ></evo-datepicker>
+            </div>
         `,
     props: {
         form,
         exampleOptions: Object.assign({...exampleOptions}, {allowInput: true, maxDate: new Date}),
-    },
+        exampleRangeOptions: Object.assign({...exampleOptions}, {
+                mode: 'range', defaultDate: ['03.08.2018', '15.08.2018'],
+                allowInput: true
+            })},
 });
 
 CSSCustomization.storyName = 'CSS customization';
