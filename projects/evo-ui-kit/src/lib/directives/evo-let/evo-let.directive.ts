8eb9dda235af48f55e6d2ff3089514a56332347c
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-03-28 15:00:15 +0300

    fix: add type guard for evoLet directive

--- projects/evo-ui-kit/src/lib/directives/evo-let/evo-let.directive.ts
+++ projects/evo-ui-kit/src/lib/directives/evo-let/evo-let.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Inject, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import {Directive, Inject, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 export class EvoLetContext<T> {
     constructor(private readonly dir: EvoLetDirective<T>) {}
@@ -23,5 +23,25 @@ export class EvoLetDirective<T> {
         @Inject(TemplateRef) templateRef: TemplateRef<EvoLetContext<T>>,
     ) {
         viewContainer.createEmbeddedView(templateRef, new EvoLetContext<T>(this));
+    }
+
+    /**
+     * Assert the correct type of the expression bound to the `evoLet` input within the template.
+     *
+     * The presence of this static field is a signal to the Ivy template type check compiler that
+     * when the `EvoLetDirective` structural directive renders its template, the type of the expression bound
+     * to `evoLet` should be narrowed in some way. For `EvoLetDirective`, the binding expression itself is used to
+     * narrow its type, which allows the strictNullChecks feature of TypeScript to work with `EvoLetDirective`.
+     */
+    static ngTemplateGuard_evoLet: 'binding';
+
+    /**
+     * Asserts the correct type of the context for the template that `EvoLetDirective` will render.
+     *
+     * The presence of this method is a signal to the Ivy template type-check compiler that the
+     * `EvoLetDirective` structural directive renders its template with a specific context type.
+     */
+    static ngTemplateContextGuard<T>(dir: EvoLetDirective<T>, ctx: any): ctx is EvoLetContext<T> {
+        return true;
     }
 }
