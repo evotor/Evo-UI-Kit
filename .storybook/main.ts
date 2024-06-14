42672a6c5fa3e79068e948eabaecd5f69b500438
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-14 15:41:14 +0300

    docs: add autodocs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,11 @@ import {StorybookConfig} from '@storybook/angular';
 const config: StorybookConfig = {
     staticDirs: ['../src/assets'],
     stories: ['../src/**/*.stories.ts'],
-    addons: ['@storybook/addon-controls', '@storybook/addon-actions'],
+    addons: ['@storybook/addon-docs', '@storybook/addon-controls', '@storybook/addon-actions'],
     framework: {
         name: '@storybook/angular',
         options: {enableIvy: true},
     },
     docs: {},
 };
-
 export default config;
