0f47bd87c3d65f3ccd190ae370610f0a46e56fca
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-20 12:06:50 +0300

    refactor(evo-input): change usage of evo-icon

--- projects/evo-ui-kit/src/lib/components/evo-input/evo-input.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-input/evo-input.module.ts
@@ -5,35 +5,12 @@ import {EvoInputComponent} from './evo-input.component';
 import {EvoUiKitModule} from '../../evo-ui-kit.module';
 import {EvoControlErrorModule} from '../evo-control-error/evo-control-error.module';
 import {FormsModule} from '@angular/forms';
-import {EvoIconModule} from '../evo-icon';
-import {iconDecline, iconHelp} from '@evotor-dev/ui-kit/icons/system';
+import {EvoIconComponent} from '../evo-icon';
 import {EvoInputIconDirective} from './evo-input-icon/evo-input-icon.directive';
 
-
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        EvoUiKitModule,
-        EvoControlErrorModule,
-        IMaskModule,
-        EvoIconModule.forRoot([{
-            name: 'icons',
-            shapes: {
-                help: iconHelp,
-                decline: iconDecline,
-            },
-        }]),
-    ],
-    declarations: [
-        EvoInputComponent,
-        EvoInputIconDirective,
-        EvoInputIconDirective,
-    ],
-    exports: [
-        EvoInputComponent,
-        EvoInputIconDirective,
-    ],
+    imports: [CommonModule, FormsModule, EvoUiKitModule, EvoControlErrorModule, IMaskModule, EvoIconComponent],
+    declarations: [EvoInputComponent, EvoInputIconDirective, EvoInputIconDirective],
+    exports: [EvoInputComponent, EvoInputIconDirective],
 })
-export class EvoInputModule {
-}
+export class EvoInputModule {}
