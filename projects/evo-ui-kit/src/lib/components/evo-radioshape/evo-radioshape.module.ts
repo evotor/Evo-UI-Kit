67b8ab922bb18d5d9a2cf27412bd55ebb1a464b7
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-24 15:32:28 +0300

    refactor(evo-radioshape): migrate to standalone

--- projects/evo-ui-kit/src/lib/components/evo-radioshape/evo-radioshape.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-radioshape/evo-radioshape.module.ts
@@ -1,11 +1,8 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {EvoRadioshapeComponent} from './components/evo-radioshape.component';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
-    declarations: [EvoRadioshapeComponent],
+    imports: [EvoRadioshapeComponent],
     exports: [EvoRadioshapeComponent],
 })
 export class EvoRadioshapeModule {}
