ed612b6bbd96337bebc8619f57030c1c4048e3e6
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-24 15:26:05 +0300

    refactor(evo-navigation-button): migrate to standalone

--- projects/evo-ui-kit/src/lib/components/evo-navigation-button/evo-navigation-button.component.ts
+++ projects/evo-ui-kit/src/lib/components/evo-navigation-button/evo-navigation-button.component.ts
@@ -1,10 +1,12 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {EvoIconComponent} from '../evo-icon/evo-icon.component';
 
 @Component({
     selector: 'evo-navigation-button',
     templateUrl: './evo-navigation-button.component.html',
     styleUrls: ['./evo-navigation-button.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [EvoIconComponent],
 })
-export class EvoNavigationButtonComponent {
-}
+export class EvoNavigationButtonComponent {}
--- projects/evo-ui-kit/src/lib/components/evo-navigation-button/evo-navigation-button.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-navigation-button/evo-navigation-button.module.ts
@@ -1,11 +1,8 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {EvoNavigationButtonComponent} from './evo-navigation-button.component';
-import {EvoIconComponent} from '../evo-icon';
 
 @NgModule({
-    imports: [CommonModule, EvoIconComponent],
-    declarations: [EvoNavigationButtonComponent],
+    imports: [EvoNavigationButtonComponent],
     exports: [EvoNavigationButtonComponent],
 })
 export class EvoNavigationButtonModule {}
