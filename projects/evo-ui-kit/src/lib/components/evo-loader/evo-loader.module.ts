5f43c00c976c471518c0730436526e9a8aded301
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-24 14:40:25 +0300

    refactor(evo-circular-loader): migrate to standalone

--- projects/evo-ui-kit/src/lib/components/evo-loader/evo-loader.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-loader/evo-loader.module.ts
@@ -1,17 +1,10 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {EvoUiKitModule} from '../../evo-ui-kit.module';
 import {EvoCircularLoaderComponent} from './evo-circular-loader.component';
 
 const bundle = [EvoCircularLoaderComponent];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        EvoUiKitModule,
-    ],
-    declarations: [...bundle],
+    imports: [...bundle],
     exports: [...bundle],
 })
-export class EvoLoaderModule {
-}
+export class EvoLoaderModule {}
