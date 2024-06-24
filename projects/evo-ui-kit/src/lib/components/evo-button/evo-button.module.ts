ebbb106e37ee889ba83b1674d97eddc9b64fd4a4
Artem Harlamenko <a.kharlamenko@evotor.ru>
2024-06-24 14:43:47 +0300

    refactor(evo-button): migrate to standalone

--- projects/evo-ui-kit/src/lib/components/evo-button/evo-button.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-button/evo-button.module.ts
@@ -1,12 +1,8 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {EvoUiKitModule} from '../../evo-ui-kit.module';
 import {EvoButtonComponent} from './components/evo-button/evo-button.component';
-import {EvoLoaderModule} from '../evo-loader';
 
 @NgModule({
-    imports: [CommonModule, EvoUiKitModule, EvoLoaderModule],
-    declarations: [EvoButtonComponent],
+    imports: [EvoButtonComponent],
     exports: [EvoButtonComponent],
 })
 export class EvoButtonModule {}
