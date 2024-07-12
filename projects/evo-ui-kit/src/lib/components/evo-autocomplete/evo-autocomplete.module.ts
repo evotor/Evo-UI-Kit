6b5a22a8c1633fd1cc73efc87ec7633fa8b77b1a
andreymatveev <a.matveev@evotor.ru>
2024-07-12 12:42:18 +0700

    feat(evo-autocomplete): update files structure

--- projects/evo-ui-kit/src/lib/components/evo-autocomplete/evo-autocomplete.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-autocomplete/evo-autocomplete.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
-import {EvoAutocompleteComponent} from './components/evo-autocomplete.component';
-import {EvoAutocompleteDefaultOptionComponent} from './components/templates/evo-autocomplete-default-option.component';
+import {EvoAutocompleteComponent} from './components/evo-autocomplete/evo-autocomplete.component';
+import {EvoAutocompleteDefaultOptionComponent} from './components/evo-autocomplete-default-option/evo-autocomplete-default-option.component';
 
 @NgModule({
     imports: [EvoAutocompleteComponent, EvoAutocompleteDefaultOptionComponent],
