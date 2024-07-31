1d808d31dde9618522d98dfe0cbc81f6268bd65f
andreymatveev <a.matveev@evotor.ru>
2024-07-31 14:16:15 +0700

    feat(evo-autocomplete): add header and footer wrappers

---
+++ projects/evo-ui-kit/src/lib/components/evo-autocomplete/components/evo-autocomplete-header/evo-autocomplete-header.component.ts
@@ -0,0 +1,10 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+    selector: 'evo-autocomplete-header',
+    templateUrl: './evo-autocomplete-header.component.html',
+    styleUrls: ['./evo-autocomplete-header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+})
+export class EvoAutocompleteHeaderComponent {}
--- projects/evo-ui-kit/src/lib/components/evo-autocomplete/evo-autocomplete.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-autocomplete/evo-autocomplete.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {EvoAutocompleteComponent} from './components/evo-autocomplete/evo-autocomplete.component';
+
 import {EvoAutocompleteDefaultOptionComponent} from './components/evo-autocomplete-default-option/evo-autocomplete-default-option.component';
+import {EvoAutocompleteHeaderComponent} from './components/evo-autocomplete-header/evo-autocomplete-header.component';
+import {EvoAutocompleteFooterComponent} from './components/evo-autocomplete-footer/evo-autocomplete-footer.component';
 
 @NgModule({
-    imports: [EvoAutocompleteComponent, EvoAutocompleteDefaultOptionComponent],
-    exports: [EvoAutocompleteComponent, EvoAutocompleteDefaultOptionComponent],
+    imports: [EvoAutocompleteComponent, EvoAutocompleteDefaultOptionComponent,EvoAutocompleteHeaderComponent,
+        EvoAutocompleteFooterComponent,],
+    exports: [EvoAutocompleteComponent, EvoAutocompleteDefaultOptionComponent,EvoAutocompleteHeaderComponent,
+        EvoAutocompleteFooterComponent,],
 })
 export class EvoAutocompleteModule {}
