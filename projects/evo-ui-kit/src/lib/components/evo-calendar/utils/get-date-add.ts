13fdb96ebe8320c99e163498afb7284c85e2c6eb
andreymatveev <a.matveev@evotor.ru>
2024-08-14 12:25:30 +0700

    feat(datepicker): new calendar and input-date (wip)

--- projects/evo-ui-kit/src/lib/components/evo-calendar/utils/get-date-add.ts
+++ projects/evo-ui-kit/src/lib/components/evo-calendar/utils/get-date-add.ts
@@ -1,6 +1,5 @@
 import {DateUnitType} from '../types';
 import {add} from 'date-fns';
-import {Duration} from 'date-fns/types';
 import {getDateFnsDuration} from './get-date-fns-duration';
 
 export function getDateAdd(date: Date, value: number, units: DateUnitType): Date {
--- projects/evo-ui-kit/src/lib/components/evo-calendar/utils/get-date-subtract.ts
+++ projects/evo-ui-kit/src/lib/components/evo-calendar/utils/get-date-subtract.ts
@@ -1,6 +1,5 @@
 import {DateUnitType} from '../types';
 import {sub} from 'date-fns';
-import {Duration} from 'date-fns/types';
 import {getDateFnsDuration} from './get-date-fns-duration';
 
 export function getDateSubtract(date: Date, value: number, units: DateUnitType): Date {
