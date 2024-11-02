f4ffac5c4ec0eb37902bb57f1eceb20ba05d8a12
a.alifanova <a.alifanova@evotor.ru>
2024-11-02 17:41:01 +0300

    feat(evo-badge): add icon directive

---
+++ projects/evo-ui-kit/src/lib/components/evo-badge/directives/evo-badge-icon.directive.spec.ts
@@ -0,0 +1,8 @@
+import {EvoBadgeIconDirective} from './evo-badge-icon.directive';
+
+describe('EvoBadgeIconDirective', () => {
+    it('should create an instance', () => {
+        const directive = new EvoBadgeIconDirective();
+        expect(directive).toBeTruthy();
+    });
+});
