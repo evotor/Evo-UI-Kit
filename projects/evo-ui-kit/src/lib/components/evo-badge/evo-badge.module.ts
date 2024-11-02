435033e677cfc2f163f00b0859fb4ef839a85b4f
a.alifanova <a.alifanova@evotor.ru>
2024-11-02 17:41:01 +0300

    feat(evo-badge): add icon directive

--- projects/evo-ui-kit/src/lib/components/evo-badge/evo-badge.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-badge/evo-badge.module.ts
@@ -2,15 +2,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EvoBadgeComponent} from './evo-badge.component';
 import {EvoUiKitModule} from '../../evo-ui-kit.module';
+import {EvoBadgeIconDirective} from "./directives/evo-badge-icon.directive";
 
 @NgModule({
-  declarations: [EvoBadgeComponent],
+  declarations: [EvoBadgeComponent, EvoBadgeIconDirective],
     imports: [
         CommonModule,
         EvoUiKitModule,
     ],
     exports: [
         EvoBadgeComponent,
+        EvoBadgeIconDirective,
     ]
 })
 export class EvoBadgeModule { }
