f4ffde59de184d8cbd01aad28582a6783e3d8bb5
a.alifanova <a.alifanova@evotor.ru>
2024-11-02 15:50:31 +0300

    feat(evo-badge): add size

--- projects/evo-ui-kit/src/lib/components/evo-badge/types/evo-badge-size.ts
+++ projects/evo-ui-kit/src/lib/components/evo-badge/types/evo-badge-size.ts
@@ -1,3 +1,3 @@
 import {EvoSize} from '../../../common/types';
 
-export type EvoBadgeSize = Extract<EvoSize, 'normal' | 'small'>;
+export type EvoBadgeSize = Extract<EvoSize, 'normal' | 'small' | 'large'>;
