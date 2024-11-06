b9214ccdb386ee6e805f12316a77d42639dd9a70
a.alifanova <a.alifanova@evotor.ru>
2024-11-06 16:08:37 +0300

    feat(evo-badge): update stories, correct styles

--- src/stories/components/evo-badge.stories.ts
+++ src/stories/components/evo-badge.stories.ts
@@ -2,13 +2,14 @@ import {moduleMetadata} from '@storybook/angular';
 import {EvoBadgeModule} from '../../../projects/evo-ui-kit/src/lib/components/evo-badge';
 import {EvoIconModule} from "@evotor-dev/ui-kit";
 import {iconStarOutlined} from "@evotor-dev/ui-kit/icons/system";
+import {EvoNoteModule} from "../../../projects/evo-ui-kit/src/lib/components/evo-note";
 
 export default {
     title: 'Components/Badge',
 
     decorators: [
         moduleMetadata({
-            imports: [EvoBadgeModule, EvoIconModule.forRoot([
+            imports: [EvoBadgeModule, EvoNoteModule, EvoIconModule.forRoot([
                 {
                     name: 'icons',
                     shapes: {
@@ -70,7 +71,12 @@ export const Custom = () => ({
             <h2>CSS Customization</h2>
             <div class="story-section">
                 <div class="badge-container">
-                    <evo-badge color="custom" style="--evo-badge-background-image: linear-gradient(87.36deg, #E4AF24 -48.16%, #ED2EAC 111.39%); --evo-badge-color: #FFFFFF">Custom background and font colors</evo-badge>
+                    <evo-badge color="custom" style="--evo-badge-background-image: linear-gradient(87.36deg, #E4AF24 -48.16%, #ED2EAC 111.39%)">Custom background</evo-badge>
+                </div>
+            </div>
+            <div class="story-section">
+                <div class="badge-container">
+                    <evo-badge color="custom" style="--evo-badge-background-image: linear-gradient(90deg, rgba(2,29,36,1) 0%, rgba(20,9,121,1) 81%, rgba(0,212,255,1) 100%); --evo-badge-color: #BEDCE3">Custom background and font</evo-badge>
                 </div>
             </div>
         </div>
@@ -121,6 +127,30 @@ export const WithFixedWidth = () => ({
                 <div class="badge-container">
                     <evo-badge [width.%]="20">20%</evo-badge>
                 </div>
+                <div class="badge-container">
+                    <evo-badge [width.%]="70">70%</evo-badge>
+                </div>
+            </div>
+            <h2>Badges with prefix icon and custom widths</h2>
+            <div class="story-section">
+                <div class="badge-container">
+                    <evo-badge [width.px]="70">
+                        <evo-icon evoBadgeIcon shape="starOutlined"></evo-icon>
+                        70px
+                    </evo-badge>
+                </div>
+                <div class="badge-container">
+                    <evo-badge [width.%]="20">
+                        <evo-icon evoBadgeIcon shape="starOutlined"></evo-icon>
+                        20%
+                    </evo-badge>
+                </div>
+                <div class="badge-container">
+                    <evo-badge [width.%]="70">
+                        <evo-icon evoBadgeIcon shape="starOutlined"></evo-icon>
+                        70%
+                    </evo-badge>
+                </div>
             </div>
         </div>
         `,
@@ -131,17 +161,50 @@ WithFixedWidth.storyName = 'with fixed width';
 export const WithMultiline = () => ({
     styleUrls: ['../../assets/scss/story-global.scss'],
     template: `
+        <evo-note type="info" style="display: block; width: 400px">
+            <p class="evo-text-header evo-text-header_h4">Внимание:</p>
+            <p>В данном режиме не поддерживается <strong>prefix icon</strong></p>
+        </evo-note>
         <div class="story-container">
             <h2>Badge multiline property</h2>
             <div class="story-section">
-                <evo-badge multiline="true" [width.px]="85">
-                    I <3️ Angular
-                    I <3️ Angular
-                    I <3️ Angular
+                <evo-badge multiline="true" size="large">
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
                 </evo-badge>
             </div>
             <div class="story-section">
-                <evo-badge multiline="true" size="small" [width.px]="85">
+                <evo-badge multiline="true" size="normal">
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                </evo-badge>
+            </div>
+            <div class="story-section">
+                <evo-badge multiline="true" size="small">
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                </evo-badge>
+            </div>
+            <h2>Badge multiline property with fixed width</h2>
+            <div class="story-section">
+                <evo-badge multiline="true" size="large" [width.px]="114">
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                </evo-badge>
+            </div>
+            <div class="story-section">
+                <evo-badge multiline="true" size="normal" [width.px]="89">
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                    I <3️ Angular<br>
+                </evo-badge>
+            </div>
+            <div class="story-section">
+                <evo-badge multiline="true" size="small" [width.px]="79">
                     I <3️ Angular<br>
                     I <3️ Angular<br>
                     I <3️ Angular<br>
