96a9c6b7a06d15b9656731efe9708f46870574f2
Andrey Matveev <andrey.g.matveev@gmail.com>
2025-06-17 12:04:43 +0700

    Revert "EVOMRKT-7845: Компоненты: пересмотреть компонент табов (v6)"

--- projects/evo-ui-kit/src/lib/components/evo-navigation-tabs/public-api.ts
+++
@@ -1,4 +0,0 @@
-export * from './evo-navigation-tabs.module';
-export * from './evo-navigation-tabs.component';
-export * from './evo-navigation-tab.directive';
-export * from "./types/evo-tabs-size";
--- projects/evo-ui-kit/src/lib/components/evo-navigation-tabs/types/evo-tabs-size.ts
+++
@@ -1,1 +0,0 @@
-export type EvoTabsSize = 'small' | 'normal';
