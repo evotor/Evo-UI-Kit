9cb8ca19ba36b385c1b0ef6f225b6420415aab55
a.kiselev <a.kiselev@evotor.ru>
2025-08-19 16:51:43 +0300

    feat(evo-tooltip): added service to public api

--- projects/evo-ui-kit/src/lib/components/evo-tooltip/public-api.ts
+++ projects/evo-ui-kit/src/lib/components/evo-tooltip/public-api.ts
@@ -3,3 +3,4 @@ export * from './directives/evo-tooltip.directive';
 export * from './types/evo-tooltip-position-type';
 export * from './interfaces/evo-tooltip-config';
 export * from './interfaces/evo-tooltip-styles';
+export * from './services/evo-tooltip.service';
