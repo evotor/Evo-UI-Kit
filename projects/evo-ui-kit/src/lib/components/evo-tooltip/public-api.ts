40725daa3e1a4ca5e7289c9d3465387378f3a7f7
a.kiselev <a.kiselev@evotor.ru>
2025-08-21 15:42:39 +0300

    feat(evo-tooltip): stories added

--- projects/evo-ui-kit/src/lib/components/evo-tooltip/public-api.ts
+++ projects/evo-ui-kit/src/lib/components/evo-tooltip/public-api.ts
@@ -1,6 +1,7 @@
 export * from './evo-tooltip.module';
 export * from './directives/evo-tooltip.directive';
 export * from './types/evo-tooltip-position-type';
+export * from './enums/evo-tooltip-position';
 export * from './interfaces/evo-tooltip-config';
 export * from './interfaces/evo-tooltip-styles';
 export * from './services/evo-tooltip.service';
