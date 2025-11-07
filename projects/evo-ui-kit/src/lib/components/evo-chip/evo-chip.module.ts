4d93186a7a27d20ab39b267b2650af774ab8d93c
a.kiselev <a.kiselev@evotor.ru>
2025-11-07 13:27:31 +0300

    feat(evo-chip): updated styles

--- projects/evo-ui-kit/src/lib/components/evo-chip/evo-chip.module.ts
+++ projects/evo-ui-kit/src/lib/components/evo-chip/evo-chip.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EvoChipComponent } from './evo-chip.component';
-import { FormsModule } from '@angular/forms';
-import { EvoUiKitModule } from '../../evo-ui-kit.module';
-import { EvoIconModule } from '../evo-icon';
-import { iconDecline } from '@evotor-dev/ui-kit/icons/system';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EvoChipComponent} from './evo-chip.component';
+import {FormsModule} from '@angular/forms';
+import {EvoUiKitModule} from '../../evo-ui-kit.module';
+import {EvoIconModule} from '../evo-icon';
+import {iconClose} from "@evotor-dev/ui-kit/icons/header";
+import {EvoCounterModule} from "../evo-counter";
 
 @NgModule({
     declarations: [EvoChipComponent],
@@ -12,14 +13,16 @@ import { iconDecline } from '@evotor-dev/ui-kit/icons/system';
         CommonModule,
         FormsModule,
         EvoUiKitModule,
-        EvoIconModule.forRoot([{
-            name: 'system',
-            shapes: {
-                'decline': iconDecline,
-            }
-        }]),
+        EvoIconModule.forRoot([
+            {
+                name: 'system',
+                shapes: {
+                    close: iconClose,
+                },
+            },
+        ]),
+        EvoCounterModule,
     ],
     exports: [EvoChipComponent],
 })
-export class EvoChipModule {
-}
+export class EvoChipModule {}
