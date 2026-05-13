8e1adbf427dbe85e6017aee5e00f0957b441704f
a.kiselev <a.kiselev@evotor.ru>
2026-05-13 19:15:20 +0300

    feat(app): add test environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {EvoTooltipModule} from '../../projects/evo-ui-kit/src/lib/components/evo-tooltip';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TestSidebarComponent} from './test-sidebar/test-sidebar.component';
+import {EvoSidebarModule} from '../../projects/evo-ui-kit/src/lib/components/evo-sidebar';
+import {EvoChipModule} from '../../projects/evo-ui-kit/src/lib/components/evo-chip';
+import {EvoDropdownModule} from '../../projects/evo-ui-kit/src/lib/components/evo-dropdown';
+import {EvoButtonModule} from '../../projects/evo-ui-kit/src/lib/components/evo-button';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, ReactiveFormsModule],
+    declarations: [AppComponent, TestSidebarComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        EvoTooltipModule,
+        BrowserAnimationsModule,
+        EvoSidebarModule,
+        EvoChipModule,
+        EvoDropdownModule,
+        EvoButtonModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
     schemas: [],
---
+++ src/app/test-sidebar/test-sidebar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestSidebarComponent } from './test-sidebar.component';
+
+describe('TestSidebarComponent', () => {
+  let component: TestSidebarComponent;
+  let fixture: ComponentFixture<TestSidebarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestSidebarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestSidebarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
