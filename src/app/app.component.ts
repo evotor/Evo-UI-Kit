449330599dff94309a1683a8dddbef56e3b1d6a5
a.kiselev <a.kiselev@evotor.ru>
2026-05-13 19:15:20 +0300

    feat(app): add test environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {EvoTooltipPosition} from '@evotor-dev/ui-kit';
+import {EvoSidebarService} from '../../projects/evo-ui-kit/src/lib/components/evo-sidebar';
+import {TestSidebarComponent} from './test-sidebar/test-sidebar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+    readonly positions = [
+        EvoTooltipPosition.TOP_START,
+        EvoTooltipPosition.TOP,
+        EvoTooltipPosition.TOP_END,
+
+        EvoTooltipPosition.BOTTOM_START,
+        EvoTooltipPosition.BOTTOM,
+        EvoTooltipPosition.BOTTOM_END,
+
+        EvoTooltipPosition.RIGHT_START,
+        EvoTooltipPosition.RIGHT,
+        EvoTooltipPosition.RIGHT_END,
+
+        EvoTooltipPosition.LEFT_START,
+        EvoTooltipPosition.LEFT,
+        EvoTooltipPosition.LEFT_END,
+    ];
+
+    constructor(private readonly evoSidebarService: EvoSidebarService) {
+        setTimeout(() => {
+            this.evoSidebarService.open({
+                component: TestSidebarComponent,
+            });
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {EvoTooltipModule} from '../../projects/evo-ui-kit/src/lib/components/evo-tooltip';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TestSidebarComponent} from './test-sidebar/test-sidebar.component';
+import {EvoChipModule} from '../../projects/evo-ui-kit/src/lib/components/evo-chip';
+import {EvoDropdownModule} from '../../projects/evo-ui-kit/src/lib/components/evo-dropdown';
+import {EvoButtonModule} from '../../projects/evo-ui-kit/src/lib/components/evo-button';
+import {
+    EvoSidebarContentComponent,
+    EvoSidebarHeaderComponent,
+    provideSidebar
+} from 'projects/evo-ui-kit/src/public_api';
+import {provideHttpClient} from "@angular/common/http";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
+    declarations: [AppComponent, TestSidebarComponent],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
+        EvoTooltipModule,
+        BrowserAnimationsModule,
+        EvoChipModule,
+        EvoDropdownModule,
+        EvoButtonModule,
+        EvoSidebarHeaderComponent,
+        EvoSidebarContentComponent,
     ],
-    providers: [],
+    providers: [provideSidebar(), provideHttpClient()],
     bootstrap: [AppComponent],
     schemas: [],
 })
